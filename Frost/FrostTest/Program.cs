c79fe14e1b1320ec0fdd011db1a7aa4e6026b358
Michael Miller <icy.arctic.fox@gmail.com>
2013-12-10 14:41:12 -0700

    Adding a test project for the Frost engine
    This project does not currently contain any unit tests. It's current
    purpose is to manually try out code.

---
+++ Frost/FrostTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace FrostTest
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			var window = new Frost.Display.Window();
+			Thread.Sleep(5000);
+		}
+	}
+}
