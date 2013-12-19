b36a582c9395abbe8b54d2bb53cea0331547ee72
Michael Miller <icy.arctic.fox@gmail.com>
2013-12-18 21:10:33 -0700

    Minor documentation additions

--- Frost/Frost/UI/Interface.cs
+++ Frost/Frost/UI/Interface.cs
@@ -4,20 +4,24 @@ using Frost.Modules.State;
 namespace Frost.UI
 {
 	/// <summary>
-	/// Top-level
+	/// Top-level interface overlay
 	/// </summary>
 	public class Interface : IStepableNode, IDrawableNode
 	{
 		/// <summary>
 		/// Updates the state of the interface
 		/// </summary>
-		/// <param name="prev"></param>
-		/// <param name="next"></param>
+		/// <param name="prev">Index of the previous state</param>
+		/// <param name="next">Index of the next state to update</param>
 		public void StepState (int prev, int next)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <summary>
+		/// Draws the state of the interface
+		/// </summary>
+		/// <param name="state">Index of the state to draw</param>
 		public void DrawState (int state)
 		{
 			throw new NotImplementedException();
