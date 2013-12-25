3c297c064ca0269f78b4f002af2031f6a05e1edc
Michael Miller <icy.arctic.fox@gmail.com>
2013-12-24 22:02:32 -0700

    Added serializable and streamable interfaces
    These interfaces represent common IO-based functionality. They will be
    used in the TNT model.

---
+++ Frost/Frost/IO/ISerializable.cs
@@ -0,0 +1,15 @@
+﻿namespace Frost.IO
+{
+	/// <summary>
+	/// An object that can serialize and deserialize itself using an array of bytes.
+	/// Serializable classes *should* have a constructor that accepts a byte array to extract content from.
+	/// </summary>
+	public interface ISerializable
+	{
+		/// <summary>
+		/// Packs up the contents of the object into an array of bytes
+		/// </summary>
+		/// <returns>Byte array containing marshaled data from the object</returns>
+		byte[] Serialize ();
+	}
+}
