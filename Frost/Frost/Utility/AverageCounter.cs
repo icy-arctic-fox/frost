d012d08482a61f1e19f79fba3b665d0f30af5446
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-04 01:07:13 -0700

    Added a running average counter
    This class can be used to average out intervals for smoother (and
    smarter) update and render synchronization.

---
+++ Frost/Frost/Utility/AverageCounter.cs
@@ -0,0 +1,53 @@
+﻿namespace Frost.Utility
+{
+	/// <summary>
+	/// Calculates the average of multiple values.
+	/// This counter tracks a maximum number of entries and wraps around - creating a running average.
+	/// </summary>
+	public class AverageCounter
+	{
+		private readonly double[] _measurements;
+		private int _pos;
+
+		/// <summary>
+		/// Creates a new average counter
+		/// </summary>
+		/// <param name="count">Maximum number of measurements to keep</param>
+		public AverageCounter (int count)
+		{
+			_measurements = new double[count];
+		}
+
+		/// <summary>
+		/// Current number of measurements
+		/// </summary>
+		public int Count { get; private set; }
+
+		/// <summary>
+		/// Average of all the measurements
+		/// </summary>
+		public double Average
+		{
+			get
+			{
+				var avg = 0d;
+				for(var i = 0; i < Count; ++i)
+					avg += _measurements[i] / Count;
+				return avg;
+			}
+		}
+
+		/// <summary>
+		/// Adds a measurement to the counter
+		/// </summary>
+		/// <param name="measurement">Measurement value</param>
+		public void AddMeasurement (double measurement)
+		{
+			_measurements[_pos++] = measurement;
+			if(_pos >= _measurements.Length)
+				_pos = 0; // Wrap around
+			else
+				++Count; // Increment number of measurements
+		}
+	}
+}
