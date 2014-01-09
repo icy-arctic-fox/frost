fc663fe069cb399befe4c56217a12ee28f1b8c02
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-09 13:47:28 -0700

    Started (very briefly) on a node type enumeration

---
+++ Frost/FrostSerialization/IO/Tnt/NodeType.cs
@@ -0,0 +1,14 @@
+﻿namespace Frost.IO.Tnt
+{
+	/// <summary>
+	/// Values for the different types of nodes that exist
+	/// </summary>
+	public enum NodeType : byte
+	{
+		/// <summary>
+		/// Not really a node type, but an indicator.
+		/// Symbolizes the end of a collection of nodes.
+		/// </summary>
+		End = 0,
+	}
+}
