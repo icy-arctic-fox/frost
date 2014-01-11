253144178d37bc334cd981f3f817ca5c2e7c3c37
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-11 16:46:35 -0700

    Started work on the resource package files

---
+++ Frost/FrostSerialization/IO/Resources/ResourcePackage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Frost.IO.Resources
+{
+	/// <summary>
+	/// Provides access to existing resource package files.
+	/// Resource packages can contain many resources of any type.
+	/// Resources contained in the package files can also be encrypted and compressed.
+	/// </summary>
+	public class ResourcePackage
+	{
+		/// <summary>
+		/// Default size of each block in bytes
+		/// </summary>
+		private const int DefaultBlockSize = 1024 * 4; // 4 KB
+
+		private readonly byte _ver;
+		private readonly int _blockSize;
+
+		/// <summary>
+		/// Version of the resource package
+		/// </summary>
+		public byte Version
+		{
+			get { return _ver; }
+		}
+
+		private ResourcePackage (byte version, int blockSize, ResourcePackageOptions options, BinaryReader br)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Frost/FrostSerialization/IO/Resources/ResourcePackageOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Frost.IO.Resources
+{
+	/// <summary>
+	/// Flags that indicate options for a resource package file
+	/// </summary>
+	[Flags]
+	public enum ResourcePackageOptions
+	{
+		/// <summary>
+		/// The header's contents are encrypted
+		/// </summary>
+		EncryptedHeader = 0x01
+	}
+}
