d71463872daecda706cfcde05ec4d8e1f2c02446
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-11 17:29:28 -0700

    Created a structure for resource package header info

--- Frost/FrostSerialization/IO/Resources/ResourcePackage.cs
+++ Frost/FrostSerialization/IO/Resources/ResourcePackage.cs
@@ -11,15 +11,9 @@ namespace Frost.IO.Resources
 	/// Resource packages can contain many resources of any type.
 	/// Resources contained in the package files can also be encrypted and compressed.
 	/// </summary>
-	public class ResourcePackage
+	public class ResourcePackage : IDisposable
 	{
-		/// <summary>
-		/// Default size of each block in bytes
-		/// </summary>
-		private const int DefaultBlockSize = 1024 * 4; // 4 KB
-
 		private readonly byte _ver;
-		private readonly int _blockSize;
 
 		/// <summary>
 		/// Version of the resource package
@@ -29,9 +23,93 @@ namespace Frost.IO.Resources
 			get { return _ver; }
 		}
 
-		private ResourcePackage (byte version, int blockSize, ResourcePackageOptions options, BinaryReader br)
+		private readonly ResourcePackageOptions _opts;
+
+		/// <summary>
+		/// Flags that specify options for the resource package
+		/// </summary>
+		public ResourcePackageOptions Options
+		{
+			get { return _opts; }
+		}
+
+		private readonly int _blockSize;
+		private readonly BinaryReader _br;
+
+		private ResourcePackage (byte ver, int blockSize, ResourcePackageOptions opts, BinaryReader br)
+		{
+			_ver       = ver;
+			_blockSize = blockSize;
+			_opts     = opts;
+			_br       = br;
+		}
+
+		#region IO
+
+		/// <summary>
+		/// Opens a resource package file to start pulling resources from it
+		/// </summary>
+		/// <param name="path">Path to the resource file</param>
+		/// <returns>A reference to a resource package file</returns>
+		/// <remarks>The file will remain open until <see cref="Close"/> or <see cref="Dispose"/> is called</remarks>
+		public static ResourcePackage Load (string path)
 		{
 			throw new NotImplementedException();
 		}
+
+		private static HeaderInfo readFileInfo (BinaryReader br)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Closes the resource package file
+		/// </summary>
+		public void Close ()
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+		#region Disposable
+
+		private volatile bool _disposed;
+
+		/// <summary>
+		/// Indicates whether the resource package has been disposed
+		/// </summary>
+		public bool Disposed
+		{
+			get { return _disposed; }
+		}
+
+		/// <summary>
+		/// Disposes of the resource package by closing the file and freeing resources
+		/// </summary>
+		public void Dispose ()
+		{
+			Dispose(true);
+		}
+
+		/// <summary>
+		/// Destructor - disposes of the resource package
+		/// </summary>
+		~ResourcePackage ()
+		{
+			Dispose(false);
+		}
+
+		/// <summary>
+		/// Disposes of the resource package
+		/// </summary>
+		/// <param name="disposing">True if inner-resources should be disposed of (<see cref="Dispose"/> was called)</param>
+		protected virtual void Dispose (bool disposing)
+		{
+			if(!_disposed)
+			{
+				_disposed = true;
+			}
+		}
+		#endregion
 	}
 }
---
+++ Frost/FrostSerialization/IO/Resources/ResourcePackageHeader.cs
@@ -0,0 +1,51 @@
+﻿namespace Frost.IO.Resources
+{
+	/// <summary>
+	/// Raw information about the structure of the file from the resource package file's header
+	/// </summary>
+	internal struct HeaderInfo
+	{
+		private readonly byte _ver;
+
+		/// <summary>
+		/// Resource package version
+		/// </summary>
+		public byte Version
+		{
+			get { return _ver; }
+		}
+
+		private readonly ResourcePackageOptions _opts;
+
+		/// <summary>
+		/// Flags that specify options for the resource package
+		/// </summary>
+		public ResourcePackageOptions Options
+		{
+			get { return _opts; }
+		}
+
+		private readonly byte _kbCount;
+
+		/// <summary>
+		/// Number of kilobytes contained in each block starting at 0 (0 means 1 KB, 255 means 256 KB)
+		/// </summary>
+		public byte KbCount
+		{
+			get { return _kbCount; }
+		}
+
+		/// <summary>
+		/// Creates new header information
+		/// </summary>
+		/// <param name="ver">Version number</param>
+		/// <param name="opts">Options for the resource package</param>
+		/// <param name="kbCount">Number of kilobytes contained in each block starting at 0 (0 means 1 KB, 255 means 256 KB)</param>
+		public HeaderInfo (byte ver, ResourcePackageOptions opts, byte kbCount)
+		{
+			_ver = ver;
+			_opts = opts;
+			_kbCount = kbCount;
+		}
+	}
+}
