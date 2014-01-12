aa792be56223a86a21a7450b601ce85b57cb0b12
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-12 11:07:04 -0700

    Changing some things around

--- Frost/TntEditor/MainForm.cs
+++ Frost/TntEditor/MainForm.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace TntEditor
 {
-	public partial class Form1 : Form
+	public partial class MainForm : Form
 	{
-		public Form1()
+		public MainForm()
 		{
 			InitializeComponent();
 		}
