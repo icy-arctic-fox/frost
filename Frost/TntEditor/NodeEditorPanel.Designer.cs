41c947c0e11be049fb9b2f0e9e1cd6cad110bac5
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-14 00:12:53 -0700

    Starting on the node editor panel
    This panel will be used to isolate node modifications from the main form
    code.

---
+++ Frost/TntEditor/NodeEditorPanel.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Frost.TntEditor
+{
+	partial class NodeEditorPanel
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose (bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent ()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NodeEditorPanel));
+			this.treeView = new System.Windows.Forms.TreeView();
+			this.nodeTypeImageList = new System.Windows.Forms.ImageList(this.components);
+			this.SuspendLayout();
+			// 
+			// treeView
+			// 
+			this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeView.ImageIndex = 0;
+			this.treeView.ImageList = this.nodeTypeImageList;
+			this.treeView.Location = new System.Drawing.Point(0, 0);
+			this.treeView.Name = "treeView";
+			this.treeView.SelectedImageIndex = 0;
+			this.treeView.Size = new System.Drawing.Size(318, 247);
+			this.treeView.TabIndex = 0;
+			// 
+			// nodeTypeImageList
+			// 
+			this.nodeTypeImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("nodeTypeImageList.ImageStream")));
+			this.nodeTypeImageList.TransparentColor = System.Drawing.Color.Transparent;
+			this.nodeTypeImageList.Images.SetKeyName(0, "type0.png");
+			this.nodeTypeImageList.Images.SetKeyName(1, "type1.png");
+			this.nodeTypeImageList.Images.SetKeyName(2, "type2.png");
+			this.nodeTypeImageList.Images.SetKeyName(3, "type3.png");
+			this.nodeTypeImageList.Images.SetKeyName(4, "type4.png");
+			this.nodeTypeImageList.Images.SetKeyName(5, "type5.png");
+			this.nodeTypeImageList.Images.SetKeyName(6, "type6.png");
+			this.nodeTypeImageList.Images.SetKeyName(7, "type7.png");
+			this.nodeTypeImageList.Images.SetKeyName(8, "type8.png");
+			this.nodeTypeImageList.Images.SetKeyName(9, "type9.png");
+			this.nodeTypeImageList.Images.SetKeyName(10, "type10.png");
+			this.nodeTypeImageList.Images.SetKeyName(11, "type11.png");
+			this.nodeTypeImageList.Images.SetKeyName(12, "type12.png");
+			this.nodeTypeImageList.Images.SetKeyName(13, "type13.png");
+			this.nodeTypeImageList.Images.SetKeyName(14, "type14.png");
+			this.nodeTypeImageList.Images.SetKeyName(15, "type15.png");
+			this.nodeTypeImageList.Images.SetKeyName(16, "type16.png");
+			this.nodeTypeImageList.Images.SetKeyName(17, "type17.png");
+			this.nodeTypeImageList.Images.SetKeyName(18, "type18.png");
+			this.nodeTypeImageList.Images.SetKeyName(19, "type19.png");
+			this.nodeTypeImageList.Images.SetKeyName(20, "type20.png");
+			this.nodeTypeImageList.Images.SetKeyName(21, "type21.png");
+			this.nodeTypeImageList.Images.SetKeyName(22, "type22.png");
+			this.nodeTypeImageList.Images.SetKeyName(23, "type23.png");
+			// 
+			// NodeEditorPanel
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.treeView);
+			this.Name = "NodeEditorPanel";
+			this.Size = new System.Drawing.Size(318, 247);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView treeView;
+		private System.Windows.Forms.ImageList nodeTypeImageList;
+	}
+}
---
+++ Frost/TntEditor/NodeEditorPanel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Frost.IO.Tnt;
+
+namespace Frost.TntEditor
+{
+	public partial class NodeEditorPanel : UserControl
+	{
+		private NodeContainer _container;
+
+		/// <summary>
+		/// Container to display the nodes of in the tree view
+		/// </summary>
+		public NodeContainer NodeContainer
+		{
+			get { return _container; }
+			set
+			{
+				_container = value;
+				if(_container != null)
+					reconstructTreeView();
+				else
+					treeView.Nodes.Clear();
+			}
+		}
+
+		public NodeEditorPanel ()
+		{
+			InitializeComponent();
+		}
+
+		#region Tree construction
+
+		/// <summary>
+		/// Reconstructs the graphical node tree
+		/// </summary>
+		private void reconstructTreeView ()
+		{
+			treeView.Nodes.Clear();
+			var rootTreeNode = constructContainerTreeNode(_container);
+
+			treeView.Nodes.Add(rootTreeNode);
+		}
+
+		/// <summary>
+		/// Constructs the top-level container tree node
+		/// </summary>
+		/// <param name="container">Container to construct a tree node for</param>
+		/// <returns>A graphical tree node</returns>
+		private static TreeNode constructContainerTreeNode (NodeContainer container)
+		{
+			var text = String.Format("Node container (version {0})", container.Version);
+			const int index = (int)NodeType.End;
+
+			return new TreeNode {
+				Text       = text,
+				ImageIndex = index,
+				Tag        = container
+			};
+		}
+
+		/// <summary>
+		/// Constructs a tree node for a node and its children
+		/// </summary>
+		/// <param name="node">Node to construct a tree node for</param>
+		/// <param name="parent">Information about the parent node</param>
+		/// <param name="name">Name to give the node (prefix in the text label)</param>
+		/// <returns>A graphical tree node</returns>
+		private static TreeNode constructTreeNode (Node node, NodeInfo parent = null, string name = null)
+		{
+			var info  = new NodeInfo(node, parent);
+			var value = node.StringValue;
+			var text  = (name == null) ? value : String.Format("{0}: {1}", name, value);
+			var type  = node.Type;
+			var index = (int)type;
+
+			var treeNode = new TreeNode {
+				Text       = text,
+				ImageIndex = index,
+				Tag        = info
+			};
+
+			switch(type)
+			{
+			case NodeType.List:
+				appendListTreeNode(treeNode, (ListNode)node, info);
+				break;
+			case NodeType.Complex:
+				appendComplexTreeNode(treeNode, (ComplexNode)node, info);
+				break;
+			}
+
+			return treeNode;
+		}
+
+		/// <summary>
+		/// Appends child nodes of a list node to a tree node
+		/// </summary>
+		/// <param name="treeNode">Tree node to append the children to</param>
+		/// <param name="nodeList">Collection of child nodes to append</param>
+		/// <param name="info">Information about the parent (list) node</param>
+		private static void appendListTreeNode (TreeNode treeNode, IEnumerable<Node> nodeList, NodeInfo info)
+		{
+			var i = 0;
+			foreach(var childNode in nodeList)
+			{
+				var name = String.Format("[{0}]", i++);
+				var childTreeNode = constructTreeNode(childNode, info, name);
+				treeNode.Nodes.Add(childTreeNode);
+			}
+		}
+
+		/// <summary>
+		/// Appends child nodes of a complex node to a tree node
+		/// </summary>
+		/// <param name="treeNode">Tree node to append the children to</param>
+		/// <param name="nodeList">Collection of child nodes to append</param>
+		/// <param name="info">Information about the parent (complex) node</param>
+		private static void appendComplexTreeNode (TreeNode treeNode, IEnumerable<KeyValuePair<string, Node>> nodeList, NodeInfo info)
+		{
+			foreach(var entry in nodeList)
+			{
+				var name      = entry.Key;
+				var childNode = entry.Value;
+				var childTreeNode = constructTreeNode(childNode, info, name);
+				treeNode.Nodes.Add(childTreeNode);
+			}
+		}
+		#endregion
+	}
+}
