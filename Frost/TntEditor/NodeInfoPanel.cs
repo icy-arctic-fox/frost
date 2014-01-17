ba9e0580c9aea6a7ec0e027fea2311832b6b2478
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-16 22:32:21 -0700

    Added an editor control for XY nodes

--- Frost/TntEditor/NodeInfoPanel.cs
+++ Frost/TntEditor/NodeInfoPanel.cs
@@ -102,6 +102,9 @@ namespace Frost.TntEditor
 			case NodeType.DateTime:
 				_nodeEditorControl = new DateTimeNodeValueControl();
 				break;
+			case NodeType.Xy:
+				_nodeEditorControl = new XyNodeValueControl();
+				break;
 			default:
 				_nodeEditorControl = null;
 				break;
---
+++ Frost/TntEditor/NodeValueControls/XyNodeValueControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+using Frost.IO.Tnt;
+
+namespace Frost.TntEditor.NodeValueControls
+{
+	public partial class XyNodeValueControl : UserControl, INodeEditorControl
+	{
+		public XyNodeValueControl ()
+		{
+			InitializeComponent();
+			xNumericUpDown.Minimum = Int32.MinValue;
+			xNumericUpDown.Maximum = Int32.MaxValue;
+			yNumericUpDown.Minimum = Int32.MinValue;
+			yNumericUpDown.Maximum = Int32.MaxValue;
+		}
+
+		/// <summary>
+		/// Gets a new node that contains the value given in the control
+		/// </summary>
+		/// <returns>A node with the entered value</returns>
+		public Node AsNode ()
+		{
+			var x = (int)xNumericUpDown.Value;
+			var y = (int)yNumericUpDown.Value;
+			return new XyNode(x, y);
+		}
+
+		/// <summary>
+		/// Sets the values contained in the control to the values from <paramref name="node"/>
+		/// </summary>
+		/// <param name="node">Node to pull values from</param>
+		public void FromNode (Node node)
+		{
+			var xy = (XyNode)node;
+			xNumericUpDown.Value = xy.X;
+			yNumericUpDown.Value = xy.Y;
+		}
+	}
+}
