ddc2f73cf4f21d57907ccc5bc4bbb7342605020a
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-19 09:36:14 -0700

    Changed around the structure for resource package
    There is now a base class for the resource package reader and writer.
    Resource package reader has been updated to reflect this change.

---
+++ Frost/FrostSerialization/IO/Resources/ResourcePackage.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frost.IO.Resources
+{
+	/// <summary>
+	/// Basic functionality for processing resource packages
+	/// </summary>
+	public abstract class ResourcePackage : IDisposable
+	{
+		protected const int Kilobyte = 1024;
+
+		/// <summary>
+		/// Version of the resource package
+		/// </summary>
+		public byte Version { get; protected set; }
+
+		/// <summary>
+		/// Flags that specify options for the resource package
+		/// </summary>
+		public ResourcePackageOptions Options { get; protected set; }
+
+		/// <summary>
+		/// Size of each file block in bytes
+		/// </summary>
+		public int BlockSize { get; protected set; }
+
+		/// <summary>
+		/// Offset in the file (measured in blocks) to where the data starts
+		/// </summary>
+		/// <remarks>This is also equal to the number of blocks used by the header.</remarks>
+		public int DataOffset { get; protected set; }
+
+		/// <summary>
+		/// Underlying stream to access the resource data
+		/// </summary>
+		protected Stream FileStream;
+
+		/// <summary>
+		/// Mapping of resource names to information about a resource in the package
+		/// </summary>
+		protected readonly Dictionary<string, ResourcePackageEntry> Entries = new Dictionary<string, ResourcePackageEntry>();
+
+		#region IO
+		#region Access
+
+		/// <summary>
+		/// Seeks to a block in the file
+		/// </summary>
+		/// <param name="block">Block index</param>
+		protected void SeekBlock (int block)
+		{
+			var offset = block * BlockSize;
+			FileStream.Seek(offset, SeekOrigin.Begin);
+		}
+
+		/// <summary>
+		/// Seeks to a data block in the file
+		/// </summary>
+		/// <param name="block">Block index</param>
+		protected void SeekDataBlock (int block)
+		{
+			SeekBlock(block + DataOffset);
+		}
+		#endregion
+
+		/// <summary>
+		/// Closes the resource package file.
+		/// No more operations to the contents of the package will be allowed for this instance.
+		/// </summary>
+		public abstract void Close ();
+		#endregion
+
+		#region Disposable
+
+		private volatile bool _disposed;
+
+		/// <summary>
+		/// Indicates whether the resource package has been disposed
+		/// </summary>
+		public bool Disposed
+		{
+			get { return _disposed; }
+			protected set { _disposed = value; }
+		}
+
+		/// <summary>
+		/// Disposes of the resource package by closing the file and freeing resources
+		/// </summary>
+		public void Dispose ()
+		{
+			Dispose(true);
+		}
+
+		/// <summary>
+		/// Destructor - disposes of the resource package
+		/// </summary>
+		~ResourcePackage ()
+		{
+			Dispose(false);
+		}
+
+		/// <summary>
+		/// Disposes of the resource package
+		/// </summary>
+		/// <param name="disposing">True if inner-resources should be disposed of (<see cref="Dispose"/> was called)</param>
+		protected abstract void Dispose (bool disposing);
+		#endregion
+	}
+}
--- Frost/FrostSerialization/IO/Resources/ResourcePackageReader.cs
+++ Frost/FrostSerialization/IO/Resources/ResourcePackageReader.cs
@@ -13,116 +13,61 @@ namespace Frost.IO.Resources
 	/// Resource packages can contain many resources of any type.
 	/// Resources contained in the package files can also be encrypted and compressed.
 	/// </summary>
-	public class ResourcePackageReader : IDisposable
+	public class ResourcePackageReader : ResourcePackage
 	{
-		private const int Kilobyte = 1024;
-
-		private readonly byte _ver;
-
-		/// <summary>
-		/// Version of the resource package
-		/// </summary>
-		public byte Version
-		{
-			get { return _ver; }
-		}
-
-		private readonly ResourcePackageOptions _opts;
-
-		/// <summary>
-		/// Flags that specify options for the resource package
-		/// </summary>
-		public ResourcePackageOptions Options
-		{
-			get { return _opts; }
-		}
-
-		/// <summary>
-		/// Size of each file block in bytes
-		/// </summary>
-		private readonly int _blockSize;
-
-		/// <summary>
-		/// Offset in the file (measured in blocks) to where the data starts
-		/// </summary>
-		/// <remarks>This is also equal to the number of blocks used by the header.</remarks>
-		private readonly int _dataOffset;
-
-		/// <summary>
-		/// Underlying stream to access the resource data
-		/// </summary>
-		private readonly Stream _s;
-
 		/// <summary>
 		/// Reader used to pull data from the file
 		/// </summary>
 		private readonly BinaryReader _br;
 
-		private ResourcePackageReader (byte ver, int blockSize, int dataOffset, ResourcePackageOptions opts, Stream s, BinaryReader br)
-		{
-			_ver        = ver;
-			_blockSize  = blockSize;
-			_dataOffset = dataOffset;
-			_opts       = opts;
-			_s          = s;
-			_br         = br;
-		}
-
-		#region Entries
-		private readonly Dictionary<string, ResourcePackageEntry> _entries = new Dictionary<string, ResourcePackageEntry>();
-		#endregion
-
-		#region IO
-		#region Load
-
 		/// <summary>
 		/// Opens a resource package file to start pulling resources from it
 		/// </summary>
-		/// <param name="path">Path to the resource file</param>
-		/// <returns>A reference to a resource package file</returns>
-		/// <exception cref="FileNotFoundException">Thrown if the resource package file wasn't found under <paramref name="path"/></exception>
+		/// <param name="filepath">Path to the resource file</param>
+		/// <exception cref="FileNotFoundException">Thrown if the resource package file wasn't found under <paramref name="filepath"/></exception>
 		/// <exception cref="InvalidDataException">Thrown if the data contained in the resource file is invalid</exception>
 		/// <remarks>The file will remain open until <see cref="Close"/> or <see cref="Dispose"/> is called</remarks>
-		public static ResourcePackageReader Load (string path)
+		public ResourcePackageReader (string filepath)
 		{
 			// Create the file stream
-			var fs = new FileStream(path, FileMode.Open);
-			var br = new EndianBinaryReader(fs, Endian.Big);
+			FileStream = new FileStream(filepath, FileMode.Open);
+			_br        = new EndianBinaryReader(FileStream, Endian.Big);
 
 			// Read the file header info
-			var fileInfo  = readFileInfo(br);
-			var blockSize = (fileInfo.KbCount + 1) * Kilobyte; // +1 to make 0 mean 1 KB block size
+			var fileInfo = readFileInfo(_br);
+			BlockSize = (fileInfo.KbCount + 1) * Kilobyte; // +1 to make 0 mean 1 KB block size
 
 			// TODO: Implement header info encryption
-			// TODO: Implement info about the package (creator, name, description, mod, etc.)
 
 			// Read the resource header information (meta-data for resources in the file)
 			NodeContainer header;
 			try
 			{
-				header = NodeContainer.ReadFromStream(br);
+				header = NodeContainer.ReadFromStream(_br);
 			}
 			catch(FormatException e)
 			{
-				br.Dispose();
+				_br.Dispose();
 				throw new InvalidDataException("The header data is in an unrecognized format.", e);
 			}
 			catch(InvalidDataException e)
 			{
-				br.Dispose();
+				_br.Dispose();
 				throw new InvalidDataException("The header data is in an unrecognized format.", e);
 			}
 
 			// Calculate how big the header is (and where the data starts)
-			var headerBytes = fs.Position;
-			var blockOffset = (int)(headerBytes / blockSize);
-			if(headerBytes % blockSize != 0)
-				++blockOffset; // Round up
+			var headerBytes = FileStream.Position;
+			DataOffset      = (int)(headerBytes / BlockSize);
+			if(headerBytes % BlockSize != 0)
+				++DataOffset; // Round up
 
-			var pkg = new ResourcePackageReader(fileInfo.Version, blockSize, blockOffset, fileInfo.Options, fs, br);
-			extractHeaderEntries(header, pkg);
-			return pkg;
+			// Pull entry information from the header
+			extractHeaderEntries(header);
 		}
+
+		#region IO
+		#region Load
 
 		/// <summary>
 		/// Reads resource package header information from the file
@@ -141,8 +86,8 @@ namespace Frost.IO.Resources
 		/// Extracts resource entries from the header and adds them to the package's records
 		/// </summary>
 		/// <param name="header">Header to extract entries from</param>
-		/// <param name="pkg">Package to add resource entries to</param>
-		private static void extractHeaderEntries (NodeContainer header, ResourcePackageReader pkg)
+		/// <exception cref="InvalidDataException">Thrown if the data contained in <paramref name="header"/> is in an unexpected format</exception>
+		private void extractHeaderEntries (NodeContainer header)
 		{
 			try
 			{
@@ -156,7 +101,7 @@ namespace Frost.IO.Resources
 					var offset = node.ExpectIntNode("offset");
 					var size   = node.ExpectIntNode("size");
 					var entry  = new ResourcePackageEntry(id, name, offset, size);
-					pkg._entries.Add(name, entry);
+					Entries.Add(name, entry);
 				}
 			}
 			catch(Exception e)
@@ -180,9 +125,9 @@ namespace Frost.IO.Resources
 				throw new ArgumentNullException("name", "The name of the resource to retrieve can't be null.");
 
 			ResourcePackageEntry entry;
-			if(_entries.TryGetValue(name, out entry)) // TODO: Add locking
+			if(Entries.TryGetValue(name, out entry)) // TODO: Add locking
 			{// Resource exists
-				seekDataBlock(entry.BlockOffset);
+				SeekDataBlock(entry.BlockOffset);
 				return readData(entry.Size);
 			}
 			return null;
@@ -230,77 +175,29 @@ namespace Frost.IO.Resources
 			}
 			return data;
 		}
-
-		/// <summary>
-		/// Seeks to a block in the file
-		/// </summary>
-		/// <param name="block">Block index</param>
-		private void seekBlock(int block)
-		{
-			var offset = block * _blockSize;
-			_s.Seek(offset, SeekOrigin.Begin);
-		}
-
-		/// <summary>
-		/// Seeks to a data block in the file
-		/// </summary>
-		/// <param name="block">Block index</param>
-		private void seekDataBlock (int block)
-		{
-			seekBlock(block + _dataOffset);
-		}
 		#endregion
 
 		/// <summary>
-		/// Closes the resource package file
+		/// Closes the resource package file.
+		/// No more operations to the contents of the package will be allowed for this instance.
 		/// </summary>
-		public void Close ()
+		public override void Close ()
 		{
 			_br.Close();
 		}
 		#endregion
 
-		#region Disposable
-
-		private volatile bool _disposed;
-
 		/// <summary>
-		/// Indicates whether the resource package reader has been disposed
-		/// </summary>
-		public bool Disposed
-		{
-			get { return _disposed; }
-		}
-
-		/// <summary>
-		/// Disposes of the resource package reader by closing the file and freeing resources
-		/// </summary>
-		public void Dispose ()
-		{
-			Dispose(true);
-		}
-
-		/// <summary>
-		/// Destructor - disposes of the resource package reader
-		/// </summary>
-		~ResourcePackageReader ()
-		{
-			Dispose(false);
-		}
-
-		/// <summary>
-		/// Disposes of the resource package reader
+		/// Disposes of the resource package
 		/// </summary>
 		/// <param name="disposing">True if inner-resources should be disposed of (<see cref="Dispose"/> was called)</param>
-		protected virtual void Dispose (bool disposing)
+		protected override void Dispose (bool disposing)
 		{
-			if(!_disposed)
+			if(!Disposed)
 			{
-				if(disposing)
-					_br.Dispose();
-				_disposed = true;
+				Disposed = true;
+				_br.Dispose();
 			}
 		}
-		#endregion
 	}
 }
