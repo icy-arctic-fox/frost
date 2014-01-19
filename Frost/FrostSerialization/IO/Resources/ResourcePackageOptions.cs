71fe5da57b7f7c26ee805f75edea7e09d9ec74cc
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-19 09:54:10 -0700

    Started implementing the package writer

--- Frost/FrostSerialization/IO/Resources/ResourcePackageOptions.cs
+++ Frost/FrostSerialization/IO/Resources/ResourcePackageOptions.cs
@@ -9,6 +9,11 @@ namespace Frost.IO.Resources
 	public enum ResourcePackageOptions : ushort
 	{
 		/// <summary>
+		/// No optional changes to the resource package file
+		/// </summary>
+		None = 0x0000,
+
+		/// <summary>
 		/// The header's contents are encrypted
 		/// </summary>
 		EncryptedHeader = 0x0001
--- Frost/FrostSerialization/IO/Resources/ResourcePackageWriter.cs
+++ Frost/FrostSerialization/IO/Resources/ResourcePackageWriter.cs
@@ -1,53 +1,84 @@
-﻿using System;
+﻿using System.IO;
 
 namespace Frost.IO.Resources
 {
 	/// <summary>
 	/// Creates resource package files
 	/// </summary>
-	public class ResourcePackageWriter : IDisposable
+	public class ResourcePackageWriter : ResourcePackage
 	{
-		#region Disposable
-
-		private volatile bool _disposed;
+		/// <summary>
+		/// Current resource package version
+		/// </summary>
+		private const int CurrentVersion = 1;
 
 		/// <summary>
-		/// Indicates whether the resource package writer has been disposed
+		/// Writer used to put data into the file
 		/// </summary>
-		public bool Disposed
+		private readonly BinaryWriter _bw;
+
+		/// <summary>
+		/// Opens a resource package file to start storing resources into it
+		/// </summary>
+		/// <param name="filepath">Path to the resource file</param>
+		/// <param name="blockSize">Size (in kilobytes) of each block minus 1 (0 means 1 KB)</param>
+		/// <param name="opts">Options for the resource package file</param>
+		/// <remarks>The file will remain open until <see cref="Close"/> or <see cref="Dispose"/> is called.</remarks>
+		public ResourcePackageWriter (string filepath, byte blockSize = 3, ResourcePackageOptions opts = ResourcePackageOptions.None)
 		{
-			get { return _disposed; }
+			// Create the file stream
+			FileStream = new FileStream(filepath, FileMode.Create);
+			_bw        = new EndianBinaryWriter(FileStream, Endian.Big);
+
+			// Write the file header info
+			var info  = new HeaderInfo(CurrentVersion, opts, blockSize);
+			BlockSize = (blockSize + 1) * Kilobyte; // +1 to make 0 mean 1 KB block size
+			Options   = opts;
+			writeFileInfo(_bw, info);
+
+			// The header is created when Write() is called
 		}
 
-		/// <summary>
-		/// Disposes of the resource package writer by flushing output, closing the file, and freeing resources
-		/// </summary>
-		public void Dispose ()
-		{
-			Dispose(true);
-		}
+		#region IO
+		#region Save
 
 		/// <summary>
-		/// Destructor - disposes of the resource package writer
+		/// Writes the resource package header to the file
 		/// </summary>
-		~ResourcePackageWriter ()
+		/// <param name="bw">Writer used to put data into the file</param>
+		/// <param name="info">Raw header information</param>
+		private static void writeFileInfo (BinaryWriter bw, HeaderInfo info)
 		{
-			Dispose(false);
-		}
-
-		/// <summary>
-		/// Disposes of the resource package writer
-		/// </summary>
-		/// <param name="disposing">True if inner-resources should be disposed of (<see cref="Dispose"/> was called)</param>
-		protected virtual void Dispose (bool disposing)
-		{
-			if(!_disposed)
-			{
-/*				if(disposing)
-					_bw.Dispose(); */
-				_disposed = true;
-			}
+			bw.Write(info.Version);
+			bw.Write((ushort)info.Options);
+			bw.Write(info.KbCount);
 		}
 		#endregion
+
+		#region Access
+		#endregion
+
+		/// <summary>
+		/// Closes the resource package file.
+		/// No more operations to the contents of the package will be allowed for this instance.
+		/// </summary>
+		public override void Close ()
+		{
+			_bw.Close();
+		}
+		#endregion
+
+		/// <summary>
+		/// Disposes of the resource package
+		/// </summary>
+		/// <param name="disposing">True if inner-resources should be disposed of (<see cref="Dispose"/> was called)</param>
+		protected override void Dispose (bool disposing)
+		{
+			if(!Disposed)
+			{
+				Disposed = true;
+				_bw.Dispose();
+			}
+		}
 	}
 }
