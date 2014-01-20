775d3e9266b484e690da0dab4b70f620f05f7537
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-20 14:01:32 -0700

    Added some documentation

--- Frost/Frost/Game.cs
+++ Frost/Frost/Game.cs
@@ -9,13 +9,28 @@ namespace Frost
 	/// </summary>
 	public abstract class Game
 	{
+		#region Game components and modules
+
+		/// <summary>
+		/// Window used to display graphics
+		/// </summary>
 		protected readonly Window Window;
 
+		/// <summary>
+		/// Manages the state and speed of the game
+		/// </summary>
 		protected readonly StateManager Manager;
 
+		/// <summary>
+		/// Provides access to all of the resources available for the game
+		/// </summary>
 		protected readonly ResourceManager Resources;
+		#endregion
 
-		public Game ()
+		/// <summary>
+		/// Creates the underlying game
+		/// </summary>
+		protected Game ()
 		{
 			throw new NotImplementedException();
 		}
