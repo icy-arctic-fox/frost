0f8f5edbead3b4e12902ef2b80dd4cdec0874f32
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-20 13:12:07 -0700

    Added documentation

--- Frost/Frost/Graphics/ResourceLoader.cs
+++ Frost/Frost/Graphics/ResourceLoader.cs
@@ -4,13 +4,29 @@ using Frost.Modules;
 
 namespace Frost.Graphics
 {
+	/// <summary>
+	/// Simplifies loading various graphical resources
+	/// </summary>
 	public static class ResourceLoader
 	{
+		/// <summary>
+		/// Passes the resource stream to a texture constructor
+		/// </summary>
+		/// <param name="info">Information about the resource</param>
+		/// <param name="s">Stream containing the texture resource</param>
+		/// <returns>The produced texture</returns>
 		private static Texture textureLoader (ResourcePackageEntry info, Stream s)
 		{
 			return new Texture(s);
 		}
 
+		/// <summary>
+		/// Loads a texture from resource packages
+		/// </summary>
+		/// <param name="manager">Manager that tracks the resource packages to load from</param>
+		/// <param name="name">Name of the texture to load</param>
+		/// <param name="allowMod">True to allow modded resources</param>
+		/// <returns>The loaded texture</returns>
 		public static Texture LoadTexture (this ResourceManager manager, string name, bool allowMod = true)
 		{
 			return manager.GetResource(name, textureLoader, allowMod);
--- Frost/Frost/Graphics/Texture.cs
+++ Frost/Frost/Graphics/Texture.cs
@@ -9,16 +9,27 @@ namespace Frost.Graphics
 	{
 		private readonly SFML.Graphics.Texture _texture;
 
+		/// <summary>
+		/// Underlying SFML texture
+		/// </summary>
 		internal SFML.Graphics.Texture InternalTexture
 		{
 			get { return _texture; }
 		}
 
+		/// <summary>
+		/// Creates a new texture from an image file
+		/// </summary>
+		/// <param name="filename">Path to the image file to load</param>
 		public Texture (string filename)
 		{
 			_texture = new SFML.Graphics.Texture(filename);
 		}
 
+		/// <summary>
+		/// Creates a new texture from an image stream
+		/// </summary>
+		/// <param name="s">Stream containing an image to load into the texture</param>
 		public Texture (Stream s)
 		{
 			_texture = new SFML.Graphics.Texture(s);
