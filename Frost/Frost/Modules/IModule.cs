a460c8cfd4835e236e7ea1c17533f4c0b19d45cb
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-23 16:21:32 -0700

    Adding a module interface

---
+++ Frost/Frost/Modules/IModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Frost.Modules
+{
+	/// <summary>
+	/// Collection of functionality with logic that runs every update
+	/// </summary>
+	public interface IModule : IDisposable
+	{
+		/// <summary>
+		/// Starts up the module and prepares it for usage
+		/// </summary>
+		void Initialize ();
+
+		/// <summary>
+		/// Performs a logic update
+		/// </summary>
+		void Update ();
+	}
+}
