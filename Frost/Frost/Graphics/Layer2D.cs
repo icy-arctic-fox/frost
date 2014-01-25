ef2a7af0d17dcc293af2658c618708f1f9c750c5
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-25 06:51:51 -0700

    Updated documentation

--- Frost/Frost/Graphics/Layer2D.cs
+++ Frost/Frost/Graphics/Layer2D.cs
@@ -13,7 +13,7 @@ namespace Frost.Graphics
 	public class Layer2D : IDisplayContainer<Object2D>
 	{
 		/// <summary>
-		/// Draws the state of a component
+		/// Draws the state of a layer and all contained objects
 		/// </summary>
 		/// <param name="display">Display to draw the state onto</param>
 		/// <param name="state">Index of the state to draw</param>
@@ -25,98 +25,91 @@ namespace Frost.Graphics
 		}
 
 		/// <summary>
-		/// Returns an enumerator that iterates through the collection.
+		/// Returns an enumerator that iterates through each <see cref="Object2D"/>
 		/// </summary>
-		/// <returns>
-		/// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
-		/// </returns>
+		/// <returns>An <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the objects</returns>
 		public IEnumerator<Object2D> GetEnumerator ()
 		{
 			throw new NotImplementedException();
 		}
 
 		/// <summary>
-		/// Returns an enumerator that iterates through a collection.
+		/// Returns an enumerator that iterates through each <see cref="Object2D"/>
 		/// </summary>
-		/// <returns>
-		/// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
-		/// </returns>
+		/// <returns>An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the objects</returns>
 		IEnumerator IEnumerable.GetEnumerator ()
 		{
 			return GetEnumerator();
 		}
 
 		/// <summary>
-		/// Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+		/// Adds an <see cref="Object2D"/> to the layer.
+		/// The <see cref="Object2D"/> will be placed on top of all existing objects.
 		/// </summary>
-		/// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param><exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.</exception>
+		/// <param name="item">The object to add to the layer</param>
 		public void Add (Object2D item)
 		{
 			throw new NotImplementedException();
 		}
 
 		/// <summary>
-		/// Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+		/// Removes all objects from the layer
 		/// </summary>
-		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only. </exception>
 		public void Clear ()
 		{
 			throw new NotImplementedException();
 		}
 
 		/// <summary>
-		/// Determines whether the <see cref="T:System.Collections.Generic.ICollection`1"/> contains a specific value.
+		/// Determines whether the layer contains a specific <see cref="Object2D"/>
 		/// </summary>
-		/// <returns>
-		/// true if <paramref name="item"/> is found in the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, false.
-		/// </returns>
-		/// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+		/// <returns>True if <paramref name="item"/> is found in the layer; otherwise, false</returns>
+		/// <param name="item">The object to locate in the layer</param>
 		public bool Contains (Object2D item)
 		{
 			throw new NotImplementedException();
 		}
 
 		/// <summary>
-		/// Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1"/> to an <see cref="T:System.Array"/>, starting at a particular <see cref="T:System.Array"/> index.
+		/// Copies the contents of the layer to an <see cref="T:System.Array"/>, starting at a specified <see cref="T:System.Array"/> index
 		/// </summary>
-		/// <param name="array">The one-dimensional <see cref="T:System.Array"/> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1"/>. The <see cref="T:System.Array"/> must have zero-based indexing.</param><param name="arrayIndex">The zero-based index in <paramref name="array"/> at which copying begins.</param><exception cref="T:System.ArgumentNullException"><paramref name="array"/> is null.</exception><exception cref="T:System.ArgumentOutOfRangeException"><paramref name="arrayIndex"/> is less than 0.</exception><exception cref="T:System.ArgumentException"><paramref name="array"/> is multidimensional.-or-The number of elements in the source <see cref="T:System.Collections.Generic.ICollection`1"/> is greater than the available space from <paramref name="arrayIndex"/> to the end of the destination <paramref name="array"/>.-or-Type <paramref name="TRenderable"/> cannot be cast automatically to the type of the destination <paramref name="array"/>.</exception>
+		/// <param name="array">The one-dimensional <see cref="T:System.Array"/> that is the destination of the <see cref="Object2D"/> copied from the layer.
+		/// The <see cref="T:System.Array"/> must have zero-based indexing.</param>
+		/// <param name="arrayIndex">The zero-based index in <paramref name="array"/> at which copying begins</param>
+		/// <exception cref="T:System.ArgumentNullException">Thrown if <paramref name="array"/> is null</exception>
+		/// <exception cref="T:System.ArgumentOutOfRangeException">Thrown if <paramref name="arrayIndex"/> is less than 0</exception>
+		/// <exception cref="T:System.ArgumentException">Thrown if <paramref name="array"/> is multidimensional or the number of objects in the layer is greater than the available space from <paramref name="arrayIndex"/> to the end of the destination <paramref name="array"/></exception>
 		public void CopyTo (Object2D[] array, int arrayIndex)
 		{
 			throw new NotImplementedException();
 		}
 
 		/// <summary>
-		/// Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+		/// Removes the first occurrence of a specific object from the layer
 		/// </summary>
-		/// <returns>
-		/// true if <paramref name="item"/> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, false. This method also returns false if <paramref name="item"/> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1"/>.
-		/// </returns>
-		/// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param><exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.</exception>
+		/// <returns>True if <paramref name="item"/> was successfully removed from the layer; otherwise, false.
+		/// This method also returns false if <paramref name="item"/> is not found in the layer.</returns>
+		/// <param name="item">The object to remove from the layer</param>
 		public bool Remove (Object2D item)
 		{
 			throw new NotImplementedException();
 		}
 
 		/// <summary>
-		/// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+		/// Number of objects contained in the layer
 		/// </summary>
-		/// <returns>
-		/// The number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"/>.
-		/// </returns>
 		public int Count
 		{
 			get { throw new NotImplementedException(); }
 		}
 
 		/// <summary>
-		/// Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+		/// Indicates whether the layer is read-only
 		/// </summary>
-		/// <returns>
-		/// true if the <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only; otherwise, false.
-		/// </returns>
+		/// <remarks>This property's value is always false.</remarks>
 		public bool IsReadOnly
 		{
-			get { throw new NotImplementedException(); }
+			get { return false; }
 		}
 	}
 }
