6be2923dfd66b6b54c02fdf32abe3334b6ed4147
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-25 06:37:05 -0700

    Changed to Layer2D
    Updated IRenderable to Object2D

--- Frost/Frost/Graphics/Layer2D.cs
+++ Frost/Frost/Graphics/Layer2D.cs
@@ -5,7 +5,12 @@ using Frost.Display;
 
 namespace Frost.Graphics
 {
-	public class RenderLayer : IDisplayContainer<IRenderable>
+	/// <summary>
+	/// A layer of graphics that contains 2D objects.
+	/// Objects are rendered in the order that they are added.
+	/// Objects added last will render on top of other objects.
+	/// </summary>
+	public class Layer2D : IDisplayContainer<Object2D>
 	{
 		/// <summary>
 		/// Draws the state of a component
@@ -25,7 +30,7 @@ namespace Frost.Graphics
 		/// <returns>
 		/// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
 		/// </returns>
-		public IEnumerator<IRenderable> GetEnumerator ()
+		public IEnumerator<Object2D> GetEnumerator ()
 		{
 			throw new NotImplementedException();
 		}
@@ -45,7 +50,7 @@ namespace Frost.Graphics
 		/// Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1"/>.
 		/// </summary>
 		/// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param><exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.</exception>
-		public void Add (IRenderable item)
+		public void Add (Object2D item)
 		{
 			throw new NotImplementedException();
 		}
@@ -66,7 +71,7 @@ namespace Frost.Graphics
 		/// true if <paramref name="item"/> is found in the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, false.
 		/// </returns>
 		/// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
-		public bool Contains (IRenderable item)
+		public bool Contains (Object2D item)
 		{
 			throw new NotImplementedException();
 		}
@@ -75,7 +80,7 @@ namespace Frost.Graphics
 		/// Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1"/> to an <see cref="T:System.Array"/>, starting at a particular <see cref="T:System.Array"/> index.
 		/// </summary>
 		/// <param name="array">The one-dimensional <see cref="T:System.Array"/> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1"/>. The <see cref="T:System.Array"/> must have zero-based indexing.</param><param name="arrayIndex">The zero-based index in <paramref name="array"/> at which copying begins.</param><exception cref="T:System.ArgumentNullException"><paramref name="array"/> is null.</exception><exception cref="T:System.ArgumentOutOfRangeException"><paramref name="arrayIndex"/> is less than 0.</exception><exception cref="T:System.ArgumentException"><paramref name="array"/> is multidimensional.-or-The number of elements in the source <see cref="T:System.Collections.Generic.ICollection`1"/> is greater than the available space from <paramref name="arrayIndex"/> to the end of the destination <paramref name="array"/>.-or-Type <paramref name="TRenderable"/> cannot be cast automatically to the type of the destination <paramref name="array"/>.</exception>
-		public void CopyTo (IRenderable[] array, int arrayIndex)
+		public void CopyTo (Object2D[] array, int arrayIndex)
 		{
 			throw new NotImplementedException();
 		}
@@ -87,7 +92,7 @@ namespace Frost.Graphics
 		/// true if <paramref name="item"/> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, false. This method also returns false if <paramref name="item"/> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1"/>.
 		/// </returns>
 		/// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param><exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.</exception>
-		public bool Remove (IRenderable item)
+		public bool Remove (Object2D item)
 		{
 			throw new NotImplementedException();
 		}
