fd756c7d76daadd83d756acdfccade0ba3b62c0e
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-25 17:52:01 -0700

    Implemented some of the mouse input functionality

---
+++ Frost/Frost/Modules/Input/MouseButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Frost.Modules.Input
+{
+	/// <summary>
+	/// Values for mouse buttons
+	/// </summary>
+	[Flags]
+	public enum MouseButton
+	{
+		/// <summary>
+		/// No mouse buttons are pressed
+		/// </summary>
+		None = 0x00,
+
+		/// <summary>
+		/// Left mouse button
+		/// </summary>
+		Left = 0x01,
+
+		/// <summary>
+		/// Right mouse button
+		/// </summary>
+		Right = 0x02,
+
+		/// <summary>
+		/// Middle mouse button
+		/// </summary>
+		Middle = 0x04
+	}
+}
