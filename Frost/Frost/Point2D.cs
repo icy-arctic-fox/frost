38e11f7404db728ba8ec7e3ddfe097b08d224d47
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-25 18:16:27 -0700

    Created a 2D point structure

---
+++ Frost/Frost/Point2D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Frost
+{
+	/// <summary>
+	/// An two-dimension point
+	/// </summary>
+	public struct Point2D
+	{
+		/// <summary>
+		/// Offset along the x-axis
+		/// </summary>
+		public readonly int X;
+
+		/// <summary>
+		/// Offset along the y-axis
+		/// </summary>
+		public readonly int Y;
+
+		/// <summary>
+		/// Creates a new point
+		/// </summary>
+		/// <param name="x">Offset along the x-axis</param>
+		/// <param name="y">Offset along the y-axis</param>
+		public Point2D (int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		/// <summary>
+		/// Calculates the distance to another point
+		/// </summary>
+		/// <param name="point">Other point</param>
+		/// <returns>Distance between the two points</returns>
+		public double DistanceTo (Point2D point)
+		{
+			var xDist = X - point.X;
+			var yDist = Y - point.Y;
+			return Math.Sqrt((xDist * xDist) + (yDist + yDist));
+		}
+	}
+}
