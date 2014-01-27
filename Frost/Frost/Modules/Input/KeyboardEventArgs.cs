ce2b1019d36f6b6e8a8c14e3f7e7def8d77fc55d
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-27 08:27:54 -0700

    Added a keyboard event class

---
+++ Frost/Frost/Modules/Input/KeyboardEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Frost.Modules.Input
+{
+	/// <summary>
+	/// Describes a keyboard event
+	/// </summary>
+	/// <remarks>Properties in this event can be modified internally.
+	/// This allows the instance to be reused since keyboard events can occur frequently.</remarks>
+	public class KeyboardEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Keyboard key that pertains to the event
+		/// </summary>
+		public Key Key { get; internal set; }
+
+		/// <summary>
+		/// Creates a new keyboard event
+		/// </summary>
+		/// <param name="key">Keyboard key that was pressed or released during the event</param>
+		public KeyboardEventArgs (Key key)
+		{
+			Key = key;
+		}
+	}
+}
