f73a3f5ccdcc3b02d6507f4399ed4f9faad265f0
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-27 14:55:38 -0700

    Adding input types enumeration

---
+++ Frost/Frost/Modules/Input/InputType.cs
@@ -0,0 +1,23 @@
+﻿namespace Frost.Modules.Input
+{
+	/// <summary>
+	/// Types of input devices
+	/// </summary>
+	public enum InputType
+	{
+		/// <summary>
+		/// Keyboard keys
+		/// </summary>
+		Keyboard,
+
+		/// <summary>
+		/// Mouse buttons and movement
+		/// </summary>
+		Mouse,
+
+		/// <summary>
+		/// Joystick positions and buttons
+		/// </summary>
+		Joystick
+	}
+}
