d9368e2a9dfae5eb5019051466664dc2e6371714
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-27 21:35:55 -0700

    Started on the input scheme
    The input scheme will be the base for mapping input to actions.

---
+++ Frost/Frost/Modules/Input/InputEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Frost.Modules.Input
+{
+	/// <summary>
+	/// Describes an event when user input has occurred
+	/// </summary>
+	/// <remarks>Properties in this class are modifiable internally to allowed a single instance to be reused.</remarks>
+	public class InputEventArgs : EventArgs
+	{
+		/// <summary>
+		/// ID of the input that occurred
+		/// </summary>
+		public int Id { get; internal set; }
+
+		/// <summary>
+		/// Amount of input for varying "strength" (e.g.: joysticks)
+		/// </summary>
+		public float Value { get; internal set; }
+	}
+}
---
+++ Frost/Frost/Modules/Input/InputScheme.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Frost.Modules.Input
+{
+	/// <summary>
+	/// Maps input to easier to recognize values
+	/// </summary>
+	public abstract class InputScheme
+	{
+		/// <summary>
+		/// Assigns an ID to an input
+		/// </summary>
+		/// <param name="id">ID to assign</param>
+		/// <param name="type">Type of input</param>
+		/// <param name="typeId">Integer value corresponding to the input (cast enum value to int)</param>
+		protected void AssignInput (int id, InputType type, int typeId)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Removes an assignment of an ID to an input
+		/// </summary>
+		/// <param name="id">ID of the input to un-assign</param>
+		protected void UnassignInput (int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Reusable event arguments since input is triggered frequently
+		/// </summary>
+		private readonly InputEventArgs _inputEventArgs = new InputEventArgs();
+
+		/// <summary>
+		/// Called when an assigned input is detected
+		/// </summary>
+		/// <param name="args">Arguments for the input</param>
+		protected void OnInputReceived (InputEventArgs args)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
