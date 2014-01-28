9d9bec48b1c67e3ee40892c5fba933141a3af287
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-28 08:43:37 -0700

    Added equality operations

--- Frost/Frost/Modules/Input/InputDescriptor.cs
+++ Frost/Frost/Modules/Input/InputDescriptor.cs
@@ -34,5 +34,61 @@
 			_type = type;
 			_id   = id;
 		}
+
+		/// <summary>
+		/// Compares two input descriptors to check if they're equal
+		/// </summary>
+		/// <param name="a">First input descriptor</param>
+		/// <param name="b">Second input descriptor</param>
+		/// <returns>True if <paramref name="a"/> and <paramref name="b"/> are equal</returns>
+		public static bool operator == (InputDescriptor a, InputDescriptor b)
+		{
+			return (a._type == b._type) && (a._id == b._id);
+		}
+
+		/// <summary>
+		/// Compares two input descriptors to check if they're not equal
+		/// </summary>
+		/// <param name="a">First input descriptor</param>
+		/// <param name="b">Second input descriptor</param>
+		/// <returns>True if <paramref name="a"/> and <paramref name="b"/> are not equal</returns>
+		public static bool operator != (InputDescriptor a, InputDescriptor b)
+		{
+			return (a._type != b._type) || (a._id != b._id);
+		}
+
+		/// <summary>
+		/// Checks if an another <see cref="InputDescriptor"/> is equal to the current instance
+		/// </summary>
+		/// <param name="other">Other <see cref="InputDescriptor"/> to compare against</param>
+		/// <returns>True if <paramref name="other"/> is effectively the same</returns>
+		public bool Equals (InputDescriptor other)
+		{
+			return (_type == other._type) && (_id == other._id);
+		}
+
+		/// <summary>
+		/// Indicates whether this instance and a specified object are equal
+		/// </summary>
+		/// <returns>True if <paramref name="obj"/> and this instance are the same type (an <see cref="InputDescriptor"/>) and represent the same value; otherwise, false</returns>
+		/// <param name="obj">Another object to compare to</param>
+		public override bool Equals (object obj)
+		{
+			if(!ReferenceEquals(null, obj))
+				return obj is InputDescriptor && Equals((InputDescriptor)obj);
+			return false;
+		}
+
+		/// <summary>
+		/// Returns the hash code for this instance
+		/// </summary>
+		/// <returns>A 32-bit signed integer that is the hash code for this instance</returns>
+		public override int GetHashCode ()
+		{
+			unchecked
+			{
+				return ((int)_type * 397) ^ _id;
+			}
+		}
 	}
 }
