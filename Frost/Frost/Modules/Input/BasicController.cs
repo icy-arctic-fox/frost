634ac451149294f963fe9ba326a869b56dbd1efb
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-28 14:04:14 -0700

    Added assignments to property setters

--- Frost/Frost/Modules/Input/BasicController.cs
+++ Frost/Frost/Modules/Input/BasicController.cs
@@ -12,6 +12,8 @@ namespace Frost.Modules.Input
 		#region Buttons
 		#region Accept button
 
+		private const int AcceptButtonId = 0;
+
 		/// <summary>
 		/// Triggered when the accept button is pressed
 		/// </summary>
@@ -22,14 +24,26 @@ namespace Frost.Modules.Input
 		/// </summary>
 		public event EventHandler<InputEventArgs> AcceptButtonReleased;
 
+		private InputDescriptor _accept;
+
 		/// <summary>
 		/// Accept, confirm, or advance button
 		/// </summary>
 		[JsonProperty("accept")]
-		public InputDescriptor AcceptButton { get; set; }
+		public InputDescriptor AcceptButton
+		{
+			get { return _accept; }
+			set
+			{
+				_accept = value;
+				AssignInput(AcceptButtonId, value);
+			}
+		}
 		#endregion
 
 		#region Cancel button
+
+		private const int CancelButtonId = 1;
 
 		/// <summary>
 		/// Triggered when the cancel button is pressed
@@ -41,14 +55,26 @@ namespace Frost.Modules.Input
 		/// </summary>
 		public event EventHandler<InputEventArgs> CancelButtonReleased;
 
+		private InputDescriptor _cancel;
+
 		/// <summary>
 		/// Cancel or confirm button
 		/// </summary>
 		[JsonProperty("cancel")]
-		public InputDescriptor CancelButton { get; set; }
+		public InputDescriptor CancelButton
+		{
+			get { return _cancel; }
+			set
+			{
+				_cancel = value;
+				AssignInput(CancelButtonId, value);
+			}
+		}
 		#endregion
 
 		#region Primary button
+
+		private const int PrimaryButtonId = 2;
 
 		/// <summary>
 		/// Triggered when the primary button is pressed
@@ -60,14 +86,26 @@ namespace Frost.Modules.Input
 		/// </summary>
 		public event EventHandler<InputEventArgs> PrimaryButtonReleased;
 
+		private InputDescriptor _primary;
+
 		/// <summary>
 		/// Primary action button
 		/// </summary>
 		[JsonProperty("primary")]
-		public InputDescriptor PrimaryButton { get; set; }
+		public InputDescriptor PrimaryButton
+		{
+			get { return _primary; }
+			set
+			{
+				_primary = value;
+				AssignInput(PrimaryButtonId, value);
+			}
+		}
 		#endregion
 
 		#region Secondary button
+
+		private const int SecondaryButtonId = 3;
 
 		/// <summary>
 		/// Triggered when the secondary button is pressed
@@ -79,16 +117,28 @@ namespace Frost.Modules.Input
 		/// </summary>
 		public event EventHandler<InputEventArgs> SecondaryButtonReleased;
 
+		private InputDescriptor _secondary;
+
 		/// <summary>
 		/// Secondary action button
 		/// </summary>
 		[JsonProperty("secondary")]
-		public InputDescriptor SecondaryButton { get; set; }
+		public InputDescriptor SecondaryButton
+		{
+			get { return _secondary; }
+			set
+			{
+				_secondary = value;
+				AssignInput(SecondaryButtonId, value);
+			}
+		}
 		#endregion
 		#endregion
 
 		#region Arrows
 		#region Up arrow
+
+		private const int UpArrowId = 4;
 
 		/// <summary>
 		/// Triggered when the up arrow is pressed
@@ -100,14 +150,26 @@ namespace Frost.Modules.Input
 		/// </summary>
 		public event EventHandler<InputEventArgs> UpArrowReleased;
 
+		private InputDescriptor _up;
+
 		/// <summary>
 		/// Up arrow button
 		/// </summary>
 		[JsonProperty("up")]
-		public InputDescriptor UpArrow { get; set; }
+		public InputDescriptor UpArrow
+		{
+			get { return _up; }
+			set
+			{
+				_up = value;
+				AssignInput(UpArrowId, value);
+			}
+		}
 		#endregion
 
 		#region Down arrow
+
+		private const int DownArrowId = 5;
 
 		/// <summary>
 		/// Triggered when the down arrow is pressed
@@ -119,14 +181,26 @@ namespace Frost.Modules.Input
 		/// </summary>
 		public event EventHandler<InputEventArgs> DownArrowReleased;
 
+		private InputDescriptor _down;
+
 		/// <summary>
 		/// Down arrow button
 		/// </summary>
 		[JsonProperty("down")]
-		public InputDescriptor DownArrow { get; set; }
+		public InputDescriptor DownArrow
+		{
+			get { return _down; }
+			set
+			{
+				_down = value;
+				AssignInput(DownArrowId, value);
+			}
+		}
 		#endregion
 
 		#region Left arrow
+
+		private const int LeftArrowId = 6;
 
 		/// <summary>
 		/// Triggered when the left arrow is pressed
@@ -138,14 +212,26 @@ namespace Frost.Modules.Input
 		/// </summary>
 		public event EventHandler<InputEventArgs> LeftArrowReleased;
 
+		private InputDescriptor _left;
+
 		/// <summary>
 		/// Left arrow button
 		/// </summary>
 		[JsonProperty("left")]
-		public InputDescriptor LeftArrow { get; set; }
+		public InputDescriptor LeftArrow
+		{
+			get { return _left; }
+			set
+			{
+				_left = value;
+				AssignInput(LeftArrowId, value);
+			}
+		}
 		#endregion
 
 		#region Right arrow
+
+		private const int RightArrowId = 7;
 
 		/// <summary>
 		/// Triggered when the right arrow is pressed
@@ -157,11 +243,21 @@ namespace Frost.Modules.Input
 		/// </summary>
 		public event EventHandler<InputEventArgs> RightArrowReleased;
 
+		private InputDescriptor _right;
+
 		/// <summary>
 		/// Right arrow button
 		/// </summary>
 		[JsonProperty("right")]
-		public InputDescriptor RightArrow { get; set; }
+		public InputDescriptor RightArrow
+		{
+			get { return _right; }
+			set
+			{
+				_right = value;
+				AssignInput(RightArrowId, value);
+			}
+		}
 		#endregion
 		#endregion
 
