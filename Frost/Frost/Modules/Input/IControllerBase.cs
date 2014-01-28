fd834240f32cc1f327a2963779b5ffd4656e34ec
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-28 08:12:02 -0700

    Added an interface for controller bases
    This interface will be useful later on for recording input.

---
+++ Frost/Frost/Modules/Input/IControllerBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Frost.Modules.Input
+{
+	/// <summary>
+	/// Very basic controller that reports when any input is detected
+	/// </summary>
+	public interface IControllerBase
+	{
+		/// <summary>
+		/// Triggered when any input is initially detected (such as a key being pressed)
+		/// </summary>
+		event EventHandler<InputEventArgs> InputStarted;
+
+		/// <summary>
+		/// Triggered when any input stops (such as a key being released)
+		/// </summary>
+		event EventHandler<InputEventArgs> InputEnded;
+	}
+}
