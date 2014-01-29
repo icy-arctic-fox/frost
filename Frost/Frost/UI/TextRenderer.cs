1557716127f39092afde6305714c09979eba819f
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-29 14:01:45 -0700

    Renaming Text to TextRenderer

---
+++ Frost/Frost/UI/TextRenderer.cs
@@ -0,0 +1,15 @@
+﻿using Frost.Display;
+using Frost.Graphics;
+using SFML.Graphics;
+using T = SFML.Graphics.Text;
+
+namespace Frost.UI
+{
+	/// <summary>
+	/// Draws text onto the screen
+	/// </summary>
+	public abstract class TextRenderer
+	{
+
+	}
+}
