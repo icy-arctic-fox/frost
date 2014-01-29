ccc30ef2b4020296cf71179e7aee126650e75719
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-29 15:52:47 -0700

    Started on LiveText documentation

--- Frost/Frost/Graphics/Text/TextRenderer.cs
+++ Frost/Frost/Graphics/Text/TextRenderer.cs
@@ -1,10 +1,16 @@
-﻿namespace Frost.Graphics.Text
+﻿using SFML.Graphics;
+
+namespace Frost.Graphics.Text
 {
 	/// <summary>
-	/// Draws text onto the screen
+	/// Draws text onto the screen.
+	/// This renderer supports 'LiveText' formatting.
 	/// </summary>
 	public abstract class TextRenderer
 	{
-
+		/// <summary>
+		/// Underlying texture used to draw text segments to
+		/// </summary>
+		private RenderTexture _texture;
 	}
 }
---
+++ Frost/Frost/LiveText.md
@@ -0,0 +1,5 @@
+LiveText Format
+===============
+
+LiveText formatting is a formatting style that uses codes to intermix with plain text. Font size, color, and style can be changed with codes. Images and text animations can also be inserted into text.
+
