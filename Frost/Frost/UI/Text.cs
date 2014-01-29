89fae3bca82ab28764427a984478b19ddf7f2bd8
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-29 13:24:51 -0700

    Minimal text class

---
+++ Frost/Frost/UI/Text.cs
@@ -0,0 +1,12 @@
+﻿using T = SFML.Graphics.Text;
+
+namespace Frost.UI
+{
+	/// <summary>
+	/// Visible text on the screen
+	/// </summary>
+	public class Text
+	{
+		private readonly T _text;
+	}
+}
