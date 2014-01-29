42c7f712ef7a08d5c6c2719678358e24c31853c3
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-29 12:42:55 -0700

    Started a font class
    Implemented the disposable interface to start with.

---
+++ Frost/Frost/UI/Font.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using F = SFML.Graphics.Font;
+
+namespace Frost.UI
+{
+	public class Font : IDisposable
+	{
+		private readonly F _font;
+
+		#region Disposabled
+		private volatile bool _disposed;
+
+		/// <summary>
+		/// Indicates whether the font has been disposed
+		/// </summary>
+		public bool Disposed
+		{
+			get { return _disposed; }
+		}
+
+		/// <summary>
+		/// Disposes of the font and the resources it holds
+		/// </summary>
+		public void Dispose ()
+		{
+			Dispose(true);
+		}
+
+		/// <summary>
+		/// Deconstructs the font
+		/// </summary>
+		~Font ()
+		{
+			Dispose(false);
+		}
+
+		/// <summary>
+		/// Disposes of the resources held by the font
+		/// </summary>
+		/// <param name="disposing">True if internal resources should also be disposed (<see cref="Dispose"/> was called)</param>
+		protected virtual void Dispose (bool disposing)
+		{
+			if(!_disposed)
+			{
+				_disposed = true;
+				if(disposing)
+					_font.Dispose();
+			}
+		}
+		#endregion
+	}
+}
