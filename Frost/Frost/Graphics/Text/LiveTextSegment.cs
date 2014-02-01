3b3da22aa07837be89cfb1e24da56605e0e12efd
Michael Miller <icy.arctic.fox@gmail.com>
2014-01-31 20:55:50 -0700

    Created part of the live text structure

---
+++ Frost/Frost/Graphics/Text/LiveTextSegment.cs
@@ -0,0 +1,15 @@
+﻿namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// A unit of live text
+	/// </summary>
+	public abstract class LiveTextSegment
+	{
+		/// <summary>
+		/// Draws the live text segment
+		/// </summary>
+		/// <param name="target">Target to draw the segment on</param>
+		/// <param name="position">Location to place the segment in the target</param>
+		public abstract void Draw (IRenderTarget target, Point2D position);
+	}
+}
---
+++ Frost/Frost/Graphics/Text/LiveTextString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// A string of text that can have formatting codes mixed throughout it to change the appearance
+	/// </summary>
+	public class LiveTextString : IEnumerable<LiveTextSegment>
+	{
+		/// <summary>
+		/// Returns an enumerator that iterates through the text segments
+		/// </summary>
+		/// <returns>A <see cref="LiveTextEnumerator"/> that can be used to iterate through the segments</returns>
+		public IEnumerator<LiveTextSegment> GetEnumerator ()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Returns an enumerator that iterates through a text segments
+		/// </summary>
+		/// <returns>An enumerator object that can be used to iterate through the collection</returns>
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return GetEnumerator();
+		}
+	}
+}
