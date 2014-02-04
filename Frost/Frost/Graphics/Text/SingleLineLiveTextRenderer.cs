fb0433bf90b9bbb18cf0fef923c580a1f6d8b142
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-04 14:09:10 -0700

    Added live text renderers

---
+++ Frost/Frost/Graphics/Text/SingleLineLiveTextRenderer.cs
@@ -0,0 +1,30 @@
+﻿using T = SFML.Graphics.Text;
+
+namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// Renders live text on a single line
+	/// </summary>
+	public class SingleLineLiveTextRenderer : LiveTextRenderer
+	{
+		private readonly T _text = new T();
+
+		/// <summary>
+		/// Displayed text
+		/// </summary>
+		public override string Text
+		{
+			get { return _text.DisplayedString; }
+			set { _text.DisplayedString = value; }
+		}
+
+		/// <summary>
+		/// Prepares the text for drawing.
+		/// This method renders the text internally so that it is ready to be quickly drawn.
+		/// </summary>
+		public override void Prepare ()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
