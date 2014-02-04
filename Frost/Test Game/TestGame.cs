6de4a92cdd122c3e5cd9be9b9566178acad7901c
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-04 14:46:09 -0700

    Added a scene to test text rendering
    This probably doesn't work...

--- Frost/Test Game/TestGame.cs
+++ Frost/Test Game/TestGame.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Frost;
+﻿using Frost;
 using Frost.Logic;
-using Frost.Modules.Input;
 
 namespace Test_Game
 {
@@ -36,51 +33,13 @@ namespace Test_Game
 			get { return 60d; }
 		}
 
-		private BallScene _scene;
-
 		/// <summary>
 		/// Creates the initial scene
 		/// </summary>
 		/// <returns>A scene</returns>
 		protected override Scene CreateInitialScene ()
 		{
-			return _scene = new BallScene();
-		}
-
-		protected override void InitializeModules ()
-		{
-			base.InitializeModules();
-			Window.MouseMove    += Mouse_Move;
-			Keyboard.KeyPress   += Keyboard_KeyPress;
-			Keyboard.KeyRelease += Keyboard_KeyRelease;
-
-			var controller = new BasicController {
-				UpArrow    = new InputDescriptor(InputType.Keyboard, (int)Key.Up),
-				DownArrow  = new InputDescriptor(InputType.Keyboard, (int)Key.Down),
-				LeftArrow  = new InputDescriptor(InputType.Keyboard, (int)Key.Left),
-				RightArrow = new InputDescriptor(InputType.Keyboard, (int)Key.Right)
-			};
-
-			_scene.SetController(controller);
-		}
-
-		private readonly HashSet<Key> _keys = new HashSet<Key>();
-
-		void Keyboard_KeyRelease (object sender, KeyboardEventArgs e)
-		{
-			_keys.Remove(e.Key);
-			Window.Title = String.Join(", ", _keys);
-		}
-
-		void Keyboard_KeyPress (object sender, KeyboardEventArgs e)
-		{
-			_keys.Add(e.Key);
-			Window.Title = String.Join(", ", _keys);
-		}
-
-		void Mouse_Move (object sender, MouseEventArgs e)
-		{
-			Window.Title = e.Position.ToString();
+			return new TextScene();
 		}
 	}
 }
---
+++ Frost/Test Game/TextScene.cs
@@ -0,0 +1,62 @@
+﻿using Frost.Display;
+using Frost.Graphics;
+using Frost.Graphics.Text;
+using Frost.Logic;
+
+namespace Test_Game
+{
+	class TextScene : Scene
+	{
+		private readonly TextSprite _ts;
+
+		/// <summary>
+		/// Creates the base of the scene
+		/// </summary>
+		public TextScene ()
+		{
+			_ts = new TextSprite("Hello!");
+			SetUpdateRoot(_ts);
+			SetRenderRoot(_ts);
+		}
+
+		private class TextSprite : IStepable, IRenderable
+		{
+			private readonly Sprite _sprite;
+
+			public TextSprite (string text)
+			{
+				using(var renderer = new SingleLinePlainTextRenderer())
+				{
+					renderer.Text = text;
+					var texture   = renderer.DrawTexture();
+					_sprite = new Sprite(texture);
+				}
+			}
+
+			/// <summary>
+			/// Updates the state of the component by a single step
+			/// </summary>
+			/// <param name="prev">Index of the previous state that was updated</param>
+			/// <param name="next">Index of the next state that should be updated</param>
+			/// <remarks>The only game state that should be modified during this process is the state indicated by <paramref name="next"/>.
+			/// The state indicated by <paramref name="prev"/> can be used for reference (if needed), but should not be modified.
+			/// Modifying any other game state info during this process would corrupt the game state.</remarks>
+			public void Step (int prev, int next)
+			{
+				_sprite.Step(prev, next);
+			}
+
+			/// <summary>
+			/// Draws the state of a component
+			/// </summary>
+			/// <param name="display">Display to draw the state onto</param>
+			/// <param name="state">Index of the state to draw</param>
+			/// <remarks>None of the game states should be modified by this process - including the state indicated by <paramref name="state"/>.
+			/// Modifying the game state info during this process would corrupt the game state.</remarks>
+			public void Draw (IDisplay display, int state)
+			{
+				_sprite.Draw(display, state);
+			}
+		}
+	}
+}
