edb3d435affe61380ff32f340ef651d0bcb20182
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-04 08:53:08 -0700

    Adding an "improved" disposable interface

---
+++ Frost/FrostSerialization/IFullDisposable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Frost
+{
+	/// <summary>
+	/// Additional information about a disposable object
+	/// </summary>
+	public interface IFullDisposable : IDisposable
+	{
+		/// <summary>
+		/// Indicates whether the object has been disposed
+		/// </summary>
+		bool Disposed { get; }
+
+		/// <summary>
+		/// Triggered when the object is being disposed
+		/// </summary>
+		event EventHandler<EventArgs> Disposing;
+	}
+}
