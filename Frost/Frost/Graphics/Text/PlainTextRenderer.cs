c716085cb4fb88a50fef4056f2e879f8c06fa7ce
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-11 19:14:06 -0700

    Started on word wrap logic
    And stopped because .NET doesn't have a common function used for linked
    lists (stupidly enough).

--- Frost/Frost/Graphics/Text/PlainTextRenderer.cs
+++ Frost/Frost/Graphics/Text/PlainTextRenderer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Frost.Graphics.Text
 {
@@ -30,31 +32,108 @@ namespace Frost.Graphics.Text
 		/// </summary>
 		public override void Prepare ()
 		{
+			var text = _text;
 			if(!MultiLine)
 			{// Strip new lines
-				var stripped =  _text.Replace("\n", String.Empty);
-				stripped = stripped.Replace("\r", String.Empty);
-				TextObject.DisplayedString = stripped;
+				text = text.Replace("\n", String.Empty);
+				text = text.Replace("\r", String.Empty);
 			}
 
 			if(WordWrap)
-				PrepareWordWrap();
+				PrepareWordWrap(text);
 			else
-				PrepareNoWrap();
+				PrepareNoWrap(text);
 		}
 
 		/// <summary>
 		/// Draws the text using word wrapping
 		/// </summary>
-		protected void PrepareWordWrap ()
+		/// <param name="text">Text to render</param>
+		protected void PrepareWordWrap (string text)
 		{
-			throw new NotImplementedException();
+			// Break apart the string at existing line breaks
+			var lines     = new LinkedList<LinkedList<string>>();
+			var newlines  = new Regex(@"\r?\n", RegexOptions.Compiled);
+			var origLines = newlines.Split(text);
+
+			// Break each line apart into words
+			var whitespace = new Regex(@"\s+", RegexOptions.Compiled);
+			foreach(var line in origLines)
+			{
+				var words = whitespace.Split(line);
+				var curLineWords = new LinkedList<string>(words);
+				lines.AddLast(curLineWords);
+			}
+
+			var curLine = lines.First;
+			while(curLine != null)
+			{
+				var pos = 0f;
+				var wordList  = curLine.Value;
+				var firstWord = wordList.First;
+				var curWord   = firstWord;
+				while(curWord != null)
+				{
+					var bounds = getTextBounds(curWord.Value);
+					pos += bounds.Width + bounds.Left;
+					if(pos > WrapLength)
+					{// Line has overflowed, push upcoming words to the next line
+						// If the current word is the first word on the line, select the next word to start at.
+						// This will prevent infinite blank lines by putting at least one word per line.
+						var startingWord = (curWord == firstWord) ? curWord : curWord.Next;
+						if(startingWord != null)
+						{// There's words to move to the next line
+//							wordList.RemoveFollowing(startingWord); // Why doesn't .NET support this on linked lists?
+						}
+					}
+					curWord = curWord.Next;
+				}
+				curLine = curLine.Next;
+			}
+
+			/*
+        
+        @lines.push([line[0]]) # Add at least one item to prevent infinte empty lines
+        line_length, _ = segment_size(line[0]) unless carry
+        
+        j, carry = 1, false
+        while j < line.count
+          s = line[j]
+          
+          # Get the width of the segment
+          w, _ = segment_size(s)
+          segment_length = w
+          
+          if w > 0 # Segment has width (text or image) - add the width of a space
+            w, _ = segment_size(' ')
+            segment_length += w
+          end
+          
+          line_length = line_length + segment_length
+          if line_length > @target_width # Exceeded the length for the line
+            items = line.slice!(j, line.count - j)
+            lines.insert(i + 1, items) # Push the segments to the next line
+            line_length = segment_length # Set this here because calling segment_size() multiple times
+            carry = true                 # on the same segment can cause problems
+            break # Not really needed, but here anyways
+          else # Segment can fit on the line
+            @lines[-1].push(' ') if w > 0 # Segment has width and needs to be prefixed with whitespace
+            @lines[-1].push(s)
+          end
+          
+          j += 1
+        end
+        
+        i += 1
+      end
+			 * */
 		}
 
 		/// <summary>
 		/// Draws the text not using word wrapping
 		/// </summary>
-		protected void PrepareNoWrap ()
+		/// <param name="text">Text to render</param>
+		protected void PrepareNoWrap (string text)
 		{
 			// Calculate the size of the text
 			var bounds = TextObject.GetLocalBounds();
@@ -64,6 +143,17 @@ namespace Frost.Graphics.Text
 
 			// Draw the text
 			Buffer.Draw(TextObject);
+		}
+
+		/// <summary>
+		/// Gets the bounds calculated for some given text
+		/// </summary>
+		/// <param name="text">Text to calculate the bounds of</param>
+		/// <returns>Text bounds</returns>
+		private SFML.Graphics.FloatRect getTextBounds (string text)
+		{
+			TextObject.DisplayedString = text;
+			return TextObject.GetLocalBounds();
 		}
 	}
 }
