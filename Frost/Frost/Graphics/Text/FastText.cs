d7980170281e129c4988068cd7066f8a12703c48
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-15 13:33:41 -0700

    Text is now displayed
    The horizonal character positioning is also updated properly as well.
    The vertical character positioning still needs to be fixed.

--- Frost/Frost/Graphics/Text/FastText.cs
+++ Frost/Frost/Graphics/Text/FastText.cs
@@ -10,8 +10,7 @@ namespace Frost.Graphics.Text
 	{
 		private readonly uint _size;
 		private readonly SFML.Graphics.Font _font;
-		private VertexArray _verts;
-		private readonly SFML.Graphics.Texture _texture;
+		private readonly VertexArray _verts;
 
 		/// <summary>
 		/// Creates a new fast text object
@@ -20,10 +19,9 @@ namespace Frost.Graphics.Text
 		/// <param name="size">Font size</param>
 		public FastText (Font font, uint size)
 		{
-			_font    = font.UnderlyingFont;
-			_size    = size;
-			_verts   = new VertexArray(PrimitiveType.Quads);
-			_texture = _font.GetTexture(size);
+			_font  = font.UnderlyingFont;
+			_size  = size;
+			_verts = new VertexArray(PrimitiveType.Quads);
 		}
 
 		private string _text;
@@ -46,15 +44,18 @@ namespace Frost.Graphics.Text
 		/// </summary>
 		private void constructVertices ()
 		{
+			// Resize the vertex array if needed
 			var textLength = _text.Length;
 			var vertCount  = (uint)textLength * 4;
 			if(_verts.VertexCount != vertCount)
 				_verts.Resize(vertCount);
+
+			var pos = 0f;
 			for(var i = 0; i < textLength; ++i)
 			{// Construct each set of 4 vertices for each glyph
 				var c = _text[i];
 				var j = (uint)(i << 2); // x4
-				constructFromGlyph(c, ref _verts, j);
+				constructFromGlyph(c, j, ref pos);
 			}
 		}
 
@@ -62,18 +63,18 @@ namespace Frost.Graphics.Text
 		/// Constructs a set of vertices from a glyph
 		/// </summary>
 		/// <param name="c">Character to get the texture of</param>
-		/// <param name="verts">Vertex array to update</param>
 		/// <param name="index">Index of the first vertex</param>
-		private void constructFromGlyph (char c, ref VertexArray verts, uint index)
+		/// <param name="pos">X-offset to place the character at and position for the next character after returning</param>
+		private void constructFromGlyph (char c, uint index, ref float pos)
 		{
 			var glyph = _font.GetGlyph(c, _size, false);
 			var rect  = glyph.TextureRect;
 
 			// Quad points
-			var v1 = new SFML.Window.Vector2f(0f,         0f);
-			var v2 = new SFML.Window.Vector2f(rect.Width, 0f);
-			var v3 = new SFML.Window.Vector2f(rect.Width, rect.Height);
-			var v4 = new SFML.Window.Vector2f(0f,         rect.Height);
+			var v1 = new SFML.Window.Vector2f(pos,              0f);
+			var v2 = new SFML.Window.Vector2f(pos + rect.Width, 0f);
+			var v3 = new SFML.Window.Vector2f(pos + rect.Width, rect.Height);
+			var v4 = new SFML.Window.Vector2f(pos,              rect.Height);
 
 			// Texture points
 			var t1 = new SFML.Window.Vector2f(rect.Left,              rect.Top);
@@ -81,10 +82,14 @@ namespace Frost.Graphics.Text
 			var t3 = new SFML.Window.Vector2f(rect.Left + rect.Width, rect.Top + rect.Height);
 			var t4 = new SFML.Window.Vector2f(rect.Left,              rect.Top + rect.Height);
 
-			verts[index++] = new Vertex(v1, t1);
-			verts[index++] = new Vertex(v2, t2);
-			verts[index++] = new Vertex(v3, t3);
-			verts[index]   = new Vertex(v4, t4);
+			// Store the vertex information
+			// TODO: Add support for color
+			_verts[index++] = new Vertex(v1, t1);
+			_verts[index++] = new Vertex(v2, t2);
+			_verts[index++] = new Vertex(v3, t3);
+			_verts[index]   = new Vertex(v4, t4);
+
+			pos += glyph.Advance;
 		}
 
 		/// <summary>
@@ -93,7 +98,7 @@ namespace Frost.Graphics.Text
 		/// <param name="target">Render target</param>
 		public void Draw (IRenderTarget target)
 		{
-			var rs = new RenderStates(_texture);
+			var rs = new RenderStates(_font.GetTexture(_size));
 			target.Draw(_verts, rs);
 		}
 	}
