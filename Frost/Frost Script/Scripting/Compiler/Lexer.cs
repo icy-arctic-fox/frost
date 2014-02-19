116b9c1c3f1e951a547d8487c33d7b8882efd76a
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-19 14:19:09 -0700

    Implemented some useful lexing methods

--- Frost/Frost Script/Scripting/Compiler/Lexer.cs
+++ Frost/Frost Script/Scripting/Compiler/Lexer.cs
@@ -13,6 +13,8 @@ namespace Frost.Scripting.Compiler
 	/// </summary>
 	public class Lexer : IFullDisposable
 	{
+		private const char NullChar = '\0';
+
 		private readonly PushbackStream _pushback;
 		private readonly BinaryReader _br;
 		private uint _line, _char;
@@ -31,7 +33,7 @@ namespace Frost.Scripting.Compiler
 			_pushback = new PushbackStream(s);
 			_br       = new BinaryReader(_pushback);
 			_line     = 1;
-			_char     = 1;
+			_char     = 0; // Initially 0 because each read increments by 1
 		}
 
 		/// <summary>
@@ -41,6 +43,118 @@ namespace Frost.Scripting.Compiler
 		public Token GetNext ()
 		{
 			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Indicates if the end of the stream has been reached
+		/// </summary>
+		private bool EndOfStream
+		{
+			get { return _pushback.Position >= _pushback.Length; }
+		}
+
+		/// <summary>
+		/// Pushes a character back onto the stream
+		/// </summary>
+		/// <param name="c">Character to push</param>
+		private void pushback (char c)
+		{
+			var bytes = BitConverter.GetBytes(c);
+			_pushback.Write(bytes, 0, bytes.Length);
+		}
+
+		/// <summary>
+		/// Clears the contents of the lexeme
+		/// </summary>
+		private void restartLexeme ()
+		{
+			_lexeme.Clear();
+		}
+
+		/// <summary>
+		/// String for the current token
+		/// </summary>
+		private string Lexeme
+		{
+			get { return _lexeme.ToString(); }
+		}
+
+		/// <summary>
+		/// Reads the next character in from the stream and advances the internal state
+		/// </summary>
+		/// <param name="c">Character read from the stream</param>
+		/// <returns>True if a character was read or false if the end of the stream was reached</returns>
+		private bool getNextChar (out char c)
+		{
+			if(!EndOfStream)
+			{// There are still characters to read
+				c = _br.ReadChar();
+				++_char;
+				_lexeme.Append(c);
+				return true;
+			}
+
+			// End of stream reached
+			c = NullChar;
+			return false;
+		}
+
+		/// <summary>
+		/// Skips over any number of whitespace characters at the current stream position
+		/// </summary>
+		/// <param name="c">First character following the whitespace</param>
+		/// <returns>True if a non-whitespace character was read or false if the end of the stream was reached before finding one</returns>
+		private bool skipWhitespace (out char c)
+		{
+			while(getNextChar(out c))
+			{// Continue reading until the end of the stream is reached
+				if(Char.IsWhiteSpace(c))
+				{
+					switch(c)
+					{// Should the line number be advanced?
+					case '\r':
+						++_line;
+						_char = 0;
+						if(getNextChar(out c) && c != '\n')
+						{// Skip \n to avoid \r\n being detected as two newlines
+							--_char;
+							pushback(c);
+						}
+						break;
+					case '\n':
+						++_line;
+						_char = 0;
+						break;
+					}
+				}
+				else
+				{// Non-whitespace character encountered
+					restartLexeme();
+					_lexeme.Append(c);
+					return true;
+				}
+			}
+
+			// End of stream reached
+			return false;
+		}
+
+		/// <summary>
+		/// Reads a character from the stream and throws an exception if it didn't match the expected character
+		/// </summary>
+		/// <param name="expected">Character that is expected to be next in the stream</param>
+		/// <exception cref="Exception">Thrown if the character read from the stream doesn't match <paramref name="expected"/>.
+		/// This is also thrown if the end of the stream was encountered.</exception>
+		private void expect (char expected)
+		{
+			char actual;
+			if(getNextChar(out actual))
+			{
+				if(actual != expected)
+					throw new Exception(String.Format("Expected {0}, but got {1}", expected, actual)); // TODO: ParserException
+			}
+			else
+				throw new Exception(String.Format("Unexpected end of file reached. Expected {0}", expected)); // TODO: ParserException
 		}
 
 		#region Disposable
