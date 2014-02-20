c9b3ef9b6b1e127dc77a5582578fb7440a588e92
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-19 20:03:16 -0700

    A bit of cleanup

--- Frost/Frost Script Test/LexerTests.cs
+++ Frost/Frost Script Test/LexerTests.cs
@@ -6,36 +6,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Frost_Script_Test
 {
+	/// <summary>
+	/// Tests the <see cref="Lexer"/> functionality
+	/// </summary>
 	[TestClass]
 	public class LexerTests
 	{
-		/// <summary>
-		/// Creates a lexer and gives it a stream containing some text
-		/// </summary>
-		/// <param name="text">Text to give the lexer</param>
-		/// <returns>A constructed lexer</returns>
-		private static Lexer setupLexer (string text)
-		{
-			var bytes = Encoding.UTF8.GetBytes(text);
-			var ms = new MemoryStream(bytes);
-			return new Lexer(ms);
-		}
-
-		/// <summary>
-		/// Asserts that a token is an integer token and has a given value
-		/// </summary>
-		/// <param name="token">Token to validate</param>
-		/// <param name="line">Line number the token should be on</param>
-		/// <param name="pos">Character position the token should start at</param>
-		/// <param name="value">Expected value of the token</param>
-		private static void assertIntegerToken (Token token, uint line, uint pos, int value)
-		{
-			Assert.IsInstanceOfType(token, typeof(IntegerToken));
-			Assert.AreEqual(line, token.Line);
-			Assert.AreEqual(pos, token.Character);
-			Assert.AreEqual(value, ((IntegerToken)token).Value);
-		}
-
 		/// <summary>
 		/// Checks if the lexer returns null for an empty string/stream
 		/// </summary>
@@ -81,6 +57,8 @@ namespace Frost_Script_Test
 			var token = lexer.GetNext();
 			assertIntegerToken(token, 1, 1, expected);
 		}
+
+		#region Binary integer literal tests
 
 		/// <summary>
 		/// Checks if the lexer handles binary integers starting with 0
@@ -164,5 +142,36 @@ namespace Frost_Script_Test
 			}
 			Assert.Fail("The lexer did not throw an exception.");
 		}
+		#endregion
+
+		#region Utility methods
+
+		/// <summary>
+		/// Creates a lexer and gives it a stream containing some text
+		/// </summary>
+		/// <param name="text">Text to give the lexer</param>
+		/// <returns>A constructed lexer</returns>
+		private static Lexer setupLexer (string text)
+		{
+			var bytes = Encoding.UTF8.GetBytes(text);
+			var ms = new MemoryStream(bytes);
+			return new Lexer(ms);
+		}
+
+		/// <summary>
+		/// Asserts that a token is an integer token and has a given value
+		/// </summary>
+		/// <param name="token">Token to validate</param>
+		/// <param name="line">Line number the token should be on</param>
+		/// <param name="pos">Character position the token should start at</param>
+		/// <param name="value">Expected value of the token</param>
+		private static void assertIntegerToken (Token token, uint line, uint pos, int value)
+		{
+			Assert.IsInstanceOfType(token, typeof(IntegerToken));
+			Assert.AreEqual(line, token.Line);
+			Assert.AreEqual(pos, token.Character);
+			Assert.AreEqual(value, ((IntegerToken)token).Value);
+		}
+		#endregion
 	}
 }
