2724b4693784628682871329c94f1b155cdb1bf0
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-19 19:59:58 -0700

    Adding some more tests

--- Frost/Frost Script Test/LexerTests.cs
+++ Frost/Frost Script Test/LexerTests.cs
@@ -48,14 +48,26 @@ namespace Frost_Script_Test
 		}
 
 		/// <summary>
+		/// Checks if the lexer returns null for a string containing only whitespace
+		/// </summary>
+		[TestMethod]
+		public void WhitespaceTest ()
+		{
+			var lexer = setupLexer(" \t\n\r\n ");
+			var token = lexer.GetNext();
+			Assert.IsNull(token);
+		}
+
+		/// <summary>
 		/// Checks if the lexer gives a correct token for just the character 0
 		/// </summary>
 		[TestMethod]
 		public void ZeroTest ()
 		{
+			const int expected = 0;
 			var lexer = setupLexer("0");
 			var token = lexer.GetNext();
-			assertIntegerToken(token, 1, 1, 0);
+			assertIntegerToken(token, 1, 1, expected);
 		}
 
 		/// <summary>
@@ -64,9 +76,10 @@ namespace Frost_Script_Test
 		[TestMethod]
 		public void SingleDigitIntegerTest ()
 		{
+			const int expected = 5;
 			var lexer = setupLexer("5");
 			var token = lexer.GetNext();
-			assertIntegerToken(token, 1, 1, 5);
+			assertIntegerToken(token, 1, 1, expected);
 		}
 
 		/// <summary>
@@ -75,10 +88,10 @@ namespace Frost_Script_Test
 		[TestMethod]
 		public void BinaryIntegerTest ()
 		{
-			const int value = 21845;
+			const int expected = 21845;
 			var lexer = setupLexer("0b0101010101010101");
 			var token = lexer.GetNext();
-			assertIntegerToken(token, 1, 1, value);
+			assertIntegerToken(token, 1, 1, expected);
 		}
 
 		/// <summary>
@@ -87,10 +100,69 @@ namespace Frost_Script_Test
 		[TestMethod]
 		public void BinaryIntegerTest2 ()
 		{
-			const int value = 21845;
+			const int expected = 21845;
 			var lexer = setupLexer("0b101010101010101");
 			var token = lexer.GetNext();
-			assertIntegerToken(token, 1, 1, value);
+			assertIntegerToken(token, 1, 1, expected);
+		}
+
+		/// <summary>
+		/// Checks if the lexer handles a binary 0 value
+		/// </summary>
+		[TestMethod]
+		public void Binary0IntegerTest ()
+		{
+			const int expected = 0;
+			var lexer = setupLexer("0b0");
+			var token = lexer.GetNext();
+			assertIntegerToken(token, 1, 1, expected);
+		}
+
+		/// <summary>
+		/// Checks if the lexer properly handles maximum binary values
+		/// </summary>
+		[TestMethod]
+		public void BigBinaryIntegerTest ()
+		{
+			const int expected = Int32.MaxValue;
+			var lexer = setupLexer("0b01111111111111111111111111111111");
+			var token = lexer.GetNext();
+			assertIntegerToken(token, 1, 1, expected);
+		}
+
+		/// <summary>
+		/// Checks if the lexer properly handles minimum binary values
+		/// </summary>
+		[TestMethod]
+		public void SmallBinaryIntegerTest ()
+		{
+			const int expected = Int32.MinValue;
+			var lexer = setupLexer("0b10000000000000000000000000000000");
+			var token = lexer.GetNext();
+			assertIntegerToken(token, 1, 1, expected);
+		}
+
+		/// <summary>
+		/// Checks if the lexer properly complains about binary integers that are too large
+		/// </summary>
+		[TestMethod]
+		public void OverflowBinaryIntegerTest ()
+		{
+			var lexer = setupLexer("0b1010101010101010101010101010101010101010101010101010101010101010");
+			try
+			{
+				lexer.GetNext();
+			}
+			catch(Exception e)
+			{
+				Assert.IsInstanceOfType(e, typeof(ParserException));
+				Assert.IsInstanceOfType(e.InnerException, typeof(OverflowException));
+				var pe = (ParserException)e;
+				Assert.Equals(1, pe.Line);
+				Assert.Equals(1, pe.Character);
+				return;
+			}
+			Assert.Fail("The lexer did not throw an exception.");
 		}
 	}
 }
