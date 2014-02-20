109354a79c92a970c075ec346c1b29e6e69376e7
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-19 17:19:42 -0700

    Created a test for a single 0 character

---
+++ Frost/Frost Script Test/LexerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Frost.Scripting.Compiler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Frost_Script_Test
+{
+	[TestClass]
+	public class LexerTests
+	{
+		/// <summary>
+		/// Creates a lexer and gives it a stream containing some text
+		/// </summary>
+		/// <param name="text">Text to give the lexer</param>
+		/// <returns>A constructed lexer</returns>
+		private static Lexer setupLexer (string text)
+		{
+			var bytes = Encoding.UTF8.GetBytes(text);
+			var ms = new MemoryStream(bytes);
+			return new Lexer(ms);
+		}
+
+		/// <summary>
+		/// Asserts that a token is an integer token and has a given value
+		/// </summary>
+		/// <param name="token">Token to validate</param>
+		/// <param name="line">Line number the token should be on</param>
+		/// <param name="pos">Character position the token should start at</param>
+		/// <param name="value">Expected value of the token</param>
+		private static void assertIntegerToken (Token token, uint line, uint pos, int value)
+		{
+			Assert.IsInstanceOfType(token, typeof(IntegerToken));
+			Assert.AreEqual(line, token.Line);
+			Assert.AreEqual(pos, token.Character);
+			Assert.AreEqual(value, ((IntegerToken)token).Value);
+		}
+
+		/// <summary>
+		/// Does the lexer give a correct token for just the character 0?
+		/// </summary>
+		[TestMethod]
+		public void ZeroTest ()
+		{
+			var lexer = setupLexer("0");
+			var token = lexer.GetNext();
+			assertIntegerToken(token, 1, 1, 0);
+		}
+	}
+}
