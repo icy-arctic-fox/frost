ff65b7782ede1da61b1e91477128519f5de57a1b
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-23 12:30:48 -0700

    Made some drawing improvements

--- Frost/Frost/UI/PixelGraph.cs
+++ Frost/Frost/UI/PixelGraph.cs
@@ -16,11 +16,11 @@ namespace Frost.UI
 	{
 		private readonly SFML.Graphics.Sprite _sprite;
 		private readonly RenderTexture _texture;
-		private readonly RectangleShape _bar;
+		private readonly Vertex[] _verts;
 
 		private readonly double _span, _height;
 		private readonly uint _width;
-		private uint _pos;
+		private float _pos;
 
 		/// <summary>
 		/// Creates a new pixel graph
@@ -39,10 +39,12 @@ namespace Frost.UI
 			_height  = height;
 			_texture = new RenderTexture(width, height);
 			_sprite  = new SFML.Graphics.Sprite(_texture.Texture);
-			_bar     = new RectangleShape();
-			_bar.FillColor = new SFML.Graphics.Color(0, 0xff, 0);
-			_bar.OutlineColor = new SFML.Graphics.Color(0, 0xff, 0);
-			_bar.OutlineThickness = 1;
+
+			// Construct vertices
+			_verts    = new Vertex[3];
+			_verts[0] = new Vertex(new SFML.Window.Vector2f(0f, 0f), new SFML.Graphics.Color(0x2e, 0xb8, 0x2e));
+			_verts[1] = new Vertex(new SFML.Window.Vector2f(0f, 0f), new SFML.Graphics.Color(0x2e, 0xb8, 0x2e, 0x00));
+			_verts[2] = new Vertex(new SFML.Window.Vector2f(0f, height), new SFML.Graphics.Color(0x00, 0x00, 0x00, 0x00));
 		}
 
 		/// <summary>
@@ -51,14 +53,18 @@ namespace Frost.UI
 		/// <param name="value">Value of the measurement</param>
 		public void AddMeasurement (double value)
 		{
-			var height    = (float)(value / _span * _height);
-			_bar.Size     = new SFML.Window.Vector2f(1f, height);
-			_bar.Position = new SFML.Window.Vector2f(_pos, (float)_height);
-			_bar.Draw(_texture, RenderStates.Default);
+			var height = value / _span * _height;
+			_verts[0].Position = new SFML.Window.Vector2f(_pos, (float)_height);
+			_verts[1].Position = new SFML.Window.Vector2f(_pos, (float)(_height - height));
+			_verts[2].Position = new SFML.Window.Vector2f(_pos, 0f);
+			_texture.Draw(_verts, PrimitiveType.LinesStrip);
 
-			++_pos;
-			if(_pos >= _width)
-				_pos = 0; // Loop around
+			_pos += 1f;
+			if(_pos > _width)
+			{// Loop around
+				_pos = 0f;
+				_texture.Clear(new SFML.Graphics.Color(0, 0, 0, 0));
+			}
 		}
 
 		/// <summary>
@@ -68,6 +74,7 @@ namespace Frost.UI
 		/// <param name="state">Index of the state to draw (ignored)</param>
 		public void Draw (IDisplay display, int state)
 		{
+			_texture.Display();
 			display.Draw(_sprite);
 		}
 	}
