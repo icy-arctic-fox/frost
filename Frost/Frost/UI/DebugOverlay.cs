47ee750888c43beb0cf1fd55be6e7f925e4a93a5
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-23 09:12:39 -0700

    Added overlay background
    More work still needs to be done with this class.

--- Frost/Frost/UI/DebugOverlay.cs
+++ Frost/Frost/UI/DebugOverlay.cs
@@ -1,4 +1,5 @@
 ﻿using Frost.Display;
+using Frost.Geometry;
 using Frost.Graphics;
 using Frost.Graphics.Text;
 using SFML.Graphics;
@@ -9,30 +10,61 @@ namespace Frost.UI
 {
 	public class DebugOverlay : IRenderable
 	{
+		private const float XPosition = 0f;
+		private const float YPosition = 0f;
+		private static readonly SFML.Graphics.Color _backgroundColor = new SFML.Graphics.Color(64, 64, 64, 128);
+
 		private readonly GameRunner _runner;
-		private readonly Shape _background;
+		private readonly SFML.Graphics.Sprite _sprite;
 		private readonly SimpleText _frameText;
 
 		public DebugOverlay (GameRunner runner)
 		{
 			_runner = runner;
-			_background = new RectangleShape(new SFML.Window.Vector2f(200f, 125f)) {
-				FillColor = new SFML.Graphics.Color(0, 0, 0, 128)
-			};
+			_sprite = new SFML.Graphics.Sprite();
 			var font   = Font.LoadFromFile("../../../../Resources/Fonts/coolvetica.ttf"); // TODO: Replace with parameter
 			var color  = new Color(255, 255, 255);
 			_frameText = new SimpleText(font, 12, color);
 		}
 
+		public Rect2f Bounds { get; private set; }
+
+		private volatile bool _resize;
+
 		public void Update ()
 		{
+			// Update the text
 			_frameText.Text = _runner.ToString();
+			var bounds = _frameText.Bounds;
+			if(bounds.Width > Bounds.Width || bounds.Height > Bounds.Height)
+			{
+				Bounds = bounds;
+				_resize = true;
+			}
 		}
 
 		public void Draw (IDisplay display, int state)
 		{
-			// TODO: Draw background
-			_frameText.Draw(display);
+			if(_sprite.Texture == null || _resize)
+			{// The background needs to be resized
+				// Calculate the bounds
+				var bounds = Bounds;
+				var width  = (uint)(bounds.Width + 1);
+				var height = (uint)(bounds.Height + 1);
+
+				// Create the new texture
+				var background  = new SFML.Graphics.Image(width, height, _backgroundColor);
+				_sprite.Texture = new SFML.Graphics.Texture(background);
+				_resize = false;
+			}
+
+			// Draw the background
+			var rs = RenderStates.Default;
+			rs.Transform.Translate(XPosition, YPosition);
+			display.Draw(_sprite, rs);
+
+			// Draw the text
+			_frameText.Draw(display, XPosition, YPosition);
 		}
 	}
 }
