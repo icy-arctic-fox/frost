d5e76c2b25cb6ff7569da11863ea0b35fcf550d3
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-23 23:59:29 -0700

    Implemented basic functionality

--- Frost/Frost/Geometry/Vector2.cs
+++ Frost/Frost/Geometry/Vector2.cs
@@ -3,37 +3,64 @@
 namespace Frost.Geometry
 {
 	/// <summary>
-	/// Defines a rotation and length
+	/// Defines a rotation and length in two-dimensions
 	/// </summary>
 	public struct Vector2
 	{
+		/// <summary>
+		/// Vector with no rotation or length
+		/// </summary>
 		public static readonly Vector2 Empty = new Vector2(0f, 0f);
 
 		private readonly float _rot, _len;
 
+		/// <summary>
+		/// Rotation in degrees
+		/// </summary>
 		public float Rotation
 		{
 			get { return _rot; }
 		}
 
+		/// <summary>
+		/// Length of the vector
+		/// </summary>
 		public float Length
 		{
 			get { return _len; }
 		}
 
+		/// <summary>
+		/// Creates a new two-dimensional vector
+		/// </summary>
+		/// <param name="rotation">Rotation measured in degrees</param>
+		/// <param name="length">Length of the vector</param>
 		public Vector2 (float rotation, float length)
 		{
-			throw new NotImplementedException();
+			_rot = rotation % 360f;
+			if(_rot < 0f)
+				_rot += 360f;
+			_len = length;
 		}
 
+		/// <summary>
+		/// Creates a new vector by applying a rotation
+		/// </summary>
+		/// <param name="angle">Angle to add in degrees</param>
+		/// <returns>A rotated vector</returns>
 		public Vector2 Rotate (float angle)
 		{
-			throw new NotImplementedException();
+			return new Vector2(_rot + angle, _len);
 		}
 
+		/// <summary>
+		/// Creates a new vector by scaling the length
+		/// </summary>
+		/// <param name="factor">Scaling factor</param>
+		/// <returns>A scaled vector</returns>
 		public Vector2 Scale (float factor)
 		{
-			throw new NotImplementedException();
+			return new Vector2(_rot, _len * factor);
 		}
 
 		#region Operators
