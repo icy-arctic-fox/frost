210717216dfc679110e00945cd483e2c0e40bedb
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-24 21:10:20 -0700

    Added a minimal entity class

---
+++ Frost/Frost/Entity.cs
@@ -0,0 +1,30 @@
+﻿namespace Frost
+{
+	/// <summary>
+	/// Base class for all entities.
+	/// An entity is a fundamental object for a game.
+	/// Entities can have any number of components (<see cref="EntityComponent"/>)
+	/// which define characteristics of that entity.
+	/// </summary>
+	public abstract class Entity
+	{
+		private readonly ulong _id;
+
+		/// <summary>
+		/// Unique entity identifier
+		/// </summary>
+		public ulong Id
+		{
+			get { return _id; }
+		}
+
+		/// <summary>
+		/// Creates a new entity
+		/// </summary>
+		/// <param name="id">Unique identifier returned from <see cref="EntityManager.GetNextAvailableId"/></param>
+		protected Entity (ulong id)
+		{
+			_id = id;
+		}
+	}
+}
