b702679cabda7481f2ec5d449d8876241c52baae
Michael Miller <icy.arctic.fox@gmail.com>
2014-02-25 16:00:46 -0700

    Created the base entity component class

---
+++ Frost/Frost/Entities/EntityComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Frost.Utility;
+
+namespace Frost.Entities
+{
+	/// <summary>
+	/// Base class for all entity components.
+	/// An entity component is a piece of functionality that an entity can have.
+	/// </summary>
+	public abstract class EntityComponent
+	{
+		/// <summary>
+		/// Triggered when any of the properties of a component are changed
+		/// </summary>
+		public event EventHandler<PropertyChangedEventArgs> PropertyChanged;
+
+		/// <summary>
+		/// This method should be called whenever a component's property has changed
+		/// </summary>
+		/// <param name="args">Event arguments</param>
+		/// <remarks>This method calls the <see cref="PropertyChanged"/> event.</remarks>
+		protected virtual void OnPropertyChanged (PropertyChangedEventArgs args)
+		{
+			PropertyChanged.NotifySubscribers(this, args);
+		}
+	}
+}
---
+++ Frost/Frost/Entities/PropertyChangedEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Frost.Entities
+{
+	/// <summary>
+	/// Describes an event when a component's property has changed
+	/// </summary>
+	public class PropertyChangedEventArgs : EventArgs
+	{
+		private readonly string _prop;
+
+		/// <summary>
+		/// Name of the property that changed
+		/// </summary>
+		public string Property
+		{
+			get { return _prop; }
+		}
+
+		/// <summary>
+		/// Creates new property changed event arguments
+		/// </summary>
+		/// <param name="prop">Name of the property that changed</param>
+		public PropertyChangedEventArgs (string prop)
+		{
+			if(String.IsNullOrWhiteSpace(prop))
+				throw new ArgumentNullException("prop", "The name of the property that changed can't be null.");
+			_prop = prop;
+		}
+	}
+}
