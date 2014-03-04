aabadd9ad36f4e8fe94ac0ebfcfee7e74038ebf8
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-04 08:30:05 -0700

    Implemented IFullDisposable

--- Frost/Frost/Entities/Entity.cs
+++ Frost/Frost/Entities/Entity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Frost.Utility;
 
 namespace Frost.Entities
 {
@@ -10,7 +11,7 @@ namespace Frost.Entities
 	/// which define characteristics of that entity.
 	/// </summary>
 	/// <seealso cref="Manager"/>
-	public partial class Entity
+	public partial class Entity : IFullDisposable
 	{
 		/// <summary>
 		/// Id of entities that have not been registered
@@ -41,5 +42,59 @@ namespace Frost.Entities
 		{
 			throw new NotImplementedException();
 		}
+
+		#region Disposable
+
+		private volatile bool _disposed;
+
+		/// <summary>
+		/// Indicates whether the entity has been freed
+		/// </summary>
+		public bool Disposed
+		{
+			get { return _disposed; }
+		}
+
+		/// <summary>
+		/// Triggered just before the entity is freed
+		/// </summary>
+		public event EventHandler<EventArgs> Disposing;
+
+		/// <summary>
+		/// Frees the resources held by the entity
+		/// </summary>
+		public void Dispose ()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Deconstructs the entity
+		/// </summary>
+		~Entity ()
+		{
+			Dispose(false);
+		}
+
+		/// <summary>
+		/// Disposes of the entity
+		/// </summary>
+		/// <param name="disposing">Flag indicating whether internal resources should be freed</param>
+		/// <remarks>This method calls the <see cref="Disposing"/> event.</remarks>
+		protected virtual void Dispose (bool disposing)
+		{
+			if(!_disposed)
+			{
+				_disposed = true;
+				Disposing.NotifySubscribers(this, EventArgs.Empty);
+
+				if(disposing)
+				{// Dispose of the internal texture
+					// TODO
+				}
+			}
+		}
+		#endregion
 	}
 }
