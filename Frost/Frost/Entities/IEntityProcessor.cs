cbd0667134e92c19f01021c698ea44ae8538d6d9
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-05 19:43:59 -0700

    Added an interface for processing entities

---
+++ Frost/Frost/Entities/IEntityProcessor.cs
@@ -0,0 +1,14 @@
+﻿namespace Frost.Entities
+{
+	/// <summary>
+	/// Processes multiple entities each frame
+	/// </summary>
+	public interface IEntityProcessor
+	{
+		/// <summary>
+		/// Processes a single entity
+		/// </summary>
+		/// <param name="e">Entity to process</param>
+		void ProcessEntity (Entity e);
+	}
+}
