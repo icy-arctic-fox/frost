4a14f4c3040e45cee32fd6f2bc30400a638bbce2
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-06 14:21:41 -0700

    Updated token validation methods

--- Frost/Frost Script Test/LexerTests.cs
+++ Frost/Frost Script Test/LexerTests.cs
@@ -606,16 +606,32 @@ namespace Frost_Script_Test
 		}
 
 		/// <summary>
+		/// Asserts that a token has a specific tag, line, and position
+		/// </summary>
+		/// <param name="token">Token to validate</param>
+		/// <param name="tag">The tag that the token should have</param>
+		/// <param name="line">Line number that the token should be on</param>
+		/// <param name="pos">Character position that the token should start at</param>
+		private static void assertToken (Token token, TokenTag tag, uint line, uint pos)
+		{
+			Assert.IsNotNull(token);
+			Assert.AreEqual(tag, token.Tag);
+			Assert.AreEqual(line, token.Line);
+			Assert.AreEqual(pos, token.Character);
+		}
+
+		/// <summary>
 		/// Asserts that a token is an integer token and has a given value
 		/// </summary>
 		/// <param name="token">Token to validate</param>
-		/// <param name="line">Line number the token should be on</param>
-		/// <param name="pos">Character position the token should start at</param>
+		/// <param name="line">Line number that the token should be on</param>
+		/// <param name="pos">Character position that the token should start at</param>
 		/// <param name="value">Expected value of the token</param>
 		/// <param name="b">Base that the integer was in</param>
 		private static void assertIntegerToken (Token token, uint line, uint pos, int value, IntegerToken.Base b)
 		{
 			Assert.IsInstanceOfType(token, typeof(IntegerToken));
+			Assert.AreEqual(TokenTag.Integer, token.Tag);
 			Assert.AreEqual(line, token.Line);
 			Assert.AreEqual(pos, token.Character);
 			Assert.AreEqual(value, ((IntegerToken)token).Value);
