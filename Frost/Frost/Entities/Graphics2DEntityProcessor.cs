ce44fc930bf25b8a2ee6e767ba91e7396912bfdc
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-05 20:29:31 -0700

    Started a 2D graphical entity processor

---
+++ Frost/Frost/Entities/Graphics2DEntityProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Frost.Entities
+{
+	/// <summary>
+	/// Draws 2D graphical entities
+	/// </summary>
+	public class Graphics2DEntityProcessor : IEntityProcessor
+	{
+		private readonly EntityComponentMap<Position2DEntityComponent> _position2DMap;
+		private readonly EntityComponentMap<TexturedEntityComponent> _texturedMap;
+
+		/// <summary>
+		/// Creates a new 2D entity graphics processor
+		/// </summary>
+		/// <param name="manager">Manager containing the entities to process</param>
+		/// <exception cref="ArgumentNullException">The entity <paramref name="manager"/> can't be null.</exception>
+		public Graphics2DEntityProcessor (Entity.Manager manager)
+		{
+			if(manager == null)
+				throw new ArgumentNullException("manager");
+
+			_position2DMap = manager.GetComponentMap<Position2DEntityComponent>();
+			_texturedMap   = manager.GetComponentMap<TexturedEntityComponent>();
+		}
+
+		/// <summary>
+		/// Draws a 2D entity
+		/// </summary>
+		/// <param name="e">Entity to process</param>
+		public void ProcessEntity (Entity e)
+		{
+			var pos = _position2DMap.GetComponent(e);
+			var tex = _texturedMap.GetComponent(e);
+
+			if(pos != null && tex != null)
+			{// Process the entity if it has the components
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
