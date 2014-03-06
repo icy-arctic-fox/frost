af4c7b9e4e327951a80851dd443526fade548be5
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-05 20:36:01 -0700

    Added the constructor and member

--- Frost/Frost/Entities/TexturedEntityComponent.cs
+++ Frost/Frost/Entities/TexturedEntityComponent.cs
@@ -1,7 +1,22 @@
-﻿namespace Frost.Entities
+﻿using Frost.Graphics;
+using Frost.Logic;
+
+namespace Frost.Entities
 {
 	public class TexturedEntityComponent : EntityComponent
 	{
+		public readonly Texture Texture;
+		public readonly StateSet<State> States;
+
+		public TexturedEntityComponent (Texture texture)
+		{
+			Texture = texture;
+			var initialStates = new State[StateManager.StateCount];
+			for(var i = 0; i < initialStates.Length; ++i)
+				initialStates[i] = new State();
+			States = new StateSet<State>(initialStates);
+		}
+
 		public class State : EntityComponentState
 		{
 			
