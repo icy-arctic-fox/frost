c8768eb6dc2a9e2b3388a4debcbfaab8f081242e
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-05 20:29:05 -0700

    Fleshed out the 2D positional component

--- Frost/Frost/Entities/Position2DEntityComponent.cs
+++ Frost/Frost/Entities/Position2DEntityComponent.cs
@@ -1,13 +1,37 @@
-﻿namespace Frost.Entities
+﻿using Frost.Logic;
+
+namespace Frost.Entities
 {
 	/// <summary>
 	/// Component that allows an entity to be represented in 2D space
 	/// </summary>
 	public class Position2DEntityComponent : EntityComponent
 	{
+		/// <summary>
+		/// State information
+		/// </summary>
+		public readonly StateSet<State> States;
+
+		/// <summary>
+		/// Creates a new 2D positional component
+		/// </summary>
+		public Position2DEntityComponent ()
+		{
+			var initialStates = new State[StateManager.StateCount];
+			for(var i = 0; i < initialStates.Length; ++i)
+				initialStates[i] = new State();
+			States = new StateSet<State>(initialStates);
+		}
+
+		/// <summary>
+		/// State information for a 2D positional entity
+		/// </summary>
 		public class State : EntityComponentState
 		{
-			private float _x, _y;
+			/// <summary>
+			/// Position of the entity in two-dimensional space
+			/// </summary>
+			public float X, Y;
 		}
 	}
 }
