b2de1ce710842942bad0ad88693e8ae3d81c596c
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-07 15:16:47 -0700

    Added a password prompt form

---
+++ Frost/Resource Packager GUI/PasswordPromptDialog.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Frost.ResourcePackagerGui
+{
+	partial class PasswordPromptDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose (bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent ()
+		{
+			this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
+			this.label = new System.Windows.Forms.Label();
+			this.lockImage = new System.Windows.Forms.PictureBox();
+			this.cancelButton = new System.Windows.Forms.Button();
+			this.okButton = new System.Windows.Forms.Button();
+			this.passwordTextBox = new System.Windows.Forms.TextBox();
+			this.tableLayout.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.lockImage)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayout
+			// 
+			this.tableLayout.ColumnCount = 3;
+			this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayout.Controls.Add(this.label, 1, 0);
+			this.tableLayout.Controls.Add(this.lockImage, 0, 0);
+			this.tableLayout.Controls.Add(this.cancelButton, 2, 0);
+			this.tableLayout.Controls.Add(this.okButton, 2, 1);
+			this.tableLayout.Controls.Add(this.passwordTextBox, 0, 1);
+			this.tableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayout.Location = new System.Drawing.Point(0, 0);
+			this.tableLayout.Name = "tableLayout";
+			this.tableLayout.RowCount = 2;
+			this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayout.Size = new System.Drawing.Size(409, 66);
+			this.tableLayout.TabIndex = 0;
+			// 
+			// label
+			// 
+			this.label.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.label.AutoSize = true;
+			this.label.Location = new System.Drawing.Point(41, 5);
+			this.label.Name = "label";
+			this.label.Size = new System.Drawing.Size(263, 26);
+			this.label.TabIndex = 0;
+			this.label.Text = "This resource package is password protected. Please enter the password to unlock " +
+    "the package.";
+			// 
+			// lockImage
+			// 
+			this.lockImage.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.lockImage.Image = global::Frost.ResourcePackagerGui.Properties.Resources._lock;
+			this.lockImage.Location = new System.Drawing.Point(3, 3);
+			this.lockImage.Name = "lockImage";
+			this.lockImage.Size = new System.Drawing.Size(32, 31);
+			this.lockImage.TabIndex = 1;
+			this.lockImage.TabStop = false;
+			// 
+			// cancelButton
+			// 
+			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.cancelButton.Location = new System.Drawing.Point(331, 11);
+			this.cancelButton.Name = "cancelButton";
+			this.cancelButton.Size = new System.Drawing.Size(75, 23);
+			this.cancelButton.TabIndex = 2;
+			this.cancelButton.Text = "Cancel";
+			this.cancelButton.UseVisualStyleBackColor = true;
+			// 
+			// okButton
+			// 
+			this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.okButton.Location = new System.Drawing.Point(331, 40);
+			this.okButton.Name = "okButton";
+			this.okButton.Size = new System.Drawing.Size(75, 23);
+			this.okButton.TabIndex = 3;
+			this.okButton.Text = "OK";
+			this.okButton.UseVisualStyleBackColor = true;
+			// 
+			// passwordTextBox
+			// 
+			this.passwordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayout.SetColumnSpan(this.passwordTextBox, 2);
+			this.passwordTextBox.Location = new System.Drawing.Point(3, 41);
+			this.passwordTextBox.Name = "passwordTextBox";
+			this.passwordTextBox.Size = new System.Drawing.Size(322, 20);
+			this.passwordTextBox.TabIndex = 4;
+			this.passwordTextBox.UseSystemPasswordChar = true;
+			// 
+			// PasswordPromptDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(409, 66);
+			this.Controls.Add(this.tableLayout);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "PasswordPromptDialog";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Password Protected";
+			this.tableLayout.ResumeLayout(false);
+			this.tableLayout.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.lockImage)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayout;
+		private System.Windows.Forms.Label label;
+		private System.Windows.Forms.PictureBox lockImage;
+		private System.Windows.Forms.Button cancelButton;
+		private System.Windows.Forms.Button okButton;
+		private System.Windows.Forms.TextBox passwordTextBox;
+	}
+}
---
+++ Frost/Resource Packager GUI/PasswordPromptDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Frost.ResourcePackagerGui
+{
+	public partial class PasswordPromptDialog : Form
+	{
+		public PasswordPromptDialog ()
+		{
+			InitializeComponent();
+		}
+	}
+}
