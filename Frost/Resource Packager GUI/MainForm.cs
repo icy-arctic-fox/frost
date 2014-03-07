184ae3946fad5c2b6d3a5e9273bf34e6a6508fd1
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-07 14:51:52 -0700

    Logic to retry loading the package

--- Frost/Resource Packager GUI/MainForm.cs
+++ Frost/Resource Packager GUI/MainForm.cs
@@ -1,20 +1,44 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Security.Cryptography;
+using System.Windows.Forms;
 using Frost.IO.Resources;
 
 namespace Frost.ResourcePackagerGui
 {
 	public partial class MainForm : Form
 	{
+		private ResourcePackage _activePackage;
+
 		public MainForm ()
 		{
 			InitializeComponent();
 		}
 
-		private void loadToolStripButton_Click (object sender, System.EventArgs e)
+		private void loadToolStripButton_Click (object sender, EventArgs args)
 		{
 			if(openFileDialog.ShowDialog() == DialogResult.OK)
 			{
-				var package = new ResourcePackageReader(openFileDialog.FileName);
+				ResourcePackage package = null;
+				string password = null;
+				var loaded = false;
+				var errorText = String.Empty;
+				do
+				{
+					try
+					{
+						package = new ResourcePackageReader(openFileDialog.FileName, password);
+						loaded = true;
+					}
+					catch(CryptographicException)
+					{// Password is incorrect
+						// TODO: Prompt for password
+					}
+					catch(Exception e)
+					{
+						errorText = String.Format("An error was encountered while attempting to load the resource package.\n\n{0}", e.Message);
+					}
+				} while(!loaded && MessageBox.Show(errorText, "Load Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry);
+				_activePackage = package;
 				resourcePackageExplorer1.DisplayPackageContents(package);
 			}
 		}
