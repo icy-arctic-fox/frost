9bc64a6c0e4e49fe0b3427fdee94c58088b95d1c
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-07 15:51:15 -0700

    Started a mock resource package class
    This class will be used to mock up a resource package and its contents
    before saving it as a real package.

---
+++ Frost/Resource Packager GUI/MockResourcePackage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Frost.IO.Resources;
+
+namespace Frost.ResourcePackagerGui
+{
+	class MockResourcePackage : IPackageInfo
+	{
+		/// <summary>
+		/// Displayed name of the package
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Information about the creator of the package.
+		/// This contains their name and possibly an email or web address.
+		/// </summary>
+		public string Creator { get; set; }
+
+		/// <summary>
+		/// Brief description of what the package is for
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Size of the package in bytes
+		/// </summary>
+		public long Size
+		{
+			get { throw new NotSupportedException(); }
+		}
+	}
+}
