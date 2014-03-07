f31f37811aa25f5abb3d6aae943481cc653a0cf1
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-07 14:56:25 -0700

    A bit of code cleanup

--- Frost/Resource Packager GUI/MainForm.cs
+++ Frost/Resource Packager GUI/MainForm.cs
@@ -14,33 +14,48 @@ namespace Frost.ResourcePackagerGui
 			InitializeComponent();
 		}
 
+		#region Save and load
+
+		private static ResourcePackage loadPackage (string filename)
+		{
+			ResourcePackage package = null;
+			string password = null;
+			var loaded = false;
+			var errorText = String.Empty;
+			do
+			{
+				try
+				{
+					package = new ResourcePackageReader(filename, password);
+					loaded = true;
+				}
+				catch(CryptographicException)
+				{// Password is incorrect
+					// TODO: Prompt for password
+				}
+				catch(Exception e)
+				{
+					errorText = String.Format("An error was encountered while attempting to load the resource package.\n\n{0}", e.Message);
+				}
+			} while(!loaded && MessageBox.Show(errorText, "Load Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry);
+			return package;
+		}
+		#endregion
+
+		#region Listeners
+
 		private void loadToolStripButton_Click (object sender, EventArgs args)
 		{
 			if(openFileDialog.ShowDialog() == DialogResult.OK)
 			{
-				ResourcePackage package = null;
-				string password = null;
-				var loaded = false;
-				var errorText = String.Empty;
-				do
+				var package = loadPackage(openFileDialog.FileName);
+				if(package != null)
 				{
-					try
-					{
-						package = new ResourcePackageReader(openFileDialog.FileName, password);
-						loaded = true;
-					}
-					catch(CryptographicException)
-					{// Password is incorrect
-						// TODO: Prompt for password
-					}
-					catch(Exception e)
-					{
-						errorText = String.Format("An error was encountered while attempting to load the resource package.\n\n{0}", e.Message);
-					}
-				} while(!loaded && MessageBox.Show(errorText, "Load Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry);
-				_activePackage = package;
-				resourcePackageExplorer1.DisplayPackageContents(package);
+					_activePackage = package;
+					resourcePackageExplorer1.DisplayPackageContents(package);
+				}
 			}
 		}
+		#endregion
 	}
 }
