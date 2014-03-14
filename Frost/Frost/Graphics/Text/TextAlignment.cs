5cfed6d989d13e5bc0fe975d486c5cbd784a55d6
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-13 21:51:58 -0600

    Added a text alignment class

---
+++ Frost/Frost/Graphics/Text/TextAlignment.cs
@@ -0,0 +1,23 @@
+﻿namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// Values for possible text alignments
+	/// </summary>
+	public enum TextAlignment
+	{
+		/// <summary>
+		/// Left-align text
+		/// </summary>
+		Left,
+
+		/// <summary>
+		/// Center-align text
+		/// </summary>
+		Center,
+
+		/// <summary>
+		/// Right-align text
+		/// </summary>
+		Right
+	}
+}
