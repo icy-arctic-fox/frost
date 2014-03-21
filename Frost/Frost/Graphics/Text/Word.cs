c439ae4f7b0917f731ef58a33063f7ca5fc75fb5
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-20 23:51:03 -0600

    Adding some initial word wrap structure

---
+++ Frost/Frost/Graphics/Text/Word.cs
@@ -0,0 +1,43 @@
+﻿using SFML.Graphics;
+
+namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// A single unbreakable segment in word wrapping
+	/// </summary>
+	/// <typeparam name="T">Type of segment</typeparam>
+	/// <seealso cref="WordWrap{T}"/>
+	internal struct Word<T>
+	{
+		private readonly T _value;
+
+		/// <summary>
+		/// Value of the word
+		/// </summary>
+		public T Value
+		{
+			get { return _value; }
+		}
+
+		private readonly IntRect _bounds;
+
+		/// <summary>
+		/// Bounds of the word
+		/// </summary>
+		public IntRect Bounds
+		{
+			get { return _bounds; }
+		}
+
+		/// <summary>
+		/// Creates a new word in a word wrap text block
+		/// </summary>
+		/// <param name="value">Value of the word</param>
+		/// <param name="bounds">Bounds of the word</param>
+		public Word (T value, IntRect bounds)
+		{
+			_value  = value;
+			_bounds = bounds;
+		}
+	}
+}
---
+++ Frost/Frost/Graphics/Text/WordWrap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Graphics;
+
+namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// Computes where line breaks should appear in a block of text
+	/// </summary>
+	internal class WordWrap<T>
+	{
+		private readonly int _width;
+		private IntRect _bounds;
+
+		/// <summary>
+		/// Creates a new word wrap processor
+		/// </summary>
+		/// <param name="width">Width to wrap each line to</param>
+		public WordWrap (int width)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Bounds that the wrapped text will occupy
+		/// </summary>
+		public IntRect Bounds
+		{
+			get { return _bounds; }
+		}
+
+		/// <summary>
+		/// Lines of text after wrapping has been applied
+		/// </summary>
+		public IEnumerable<Word<T>[]> Lines
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Total number of lines of text after wrapping has been applied
+		/// </summary>
+		public int LineCount { get; private set; }
+
+		/// <summary>
+		/// Adds a unbreakable segment to the end of the text
+		/// </summary>
+		/// <param name="word">Segment to </param>
+		public void Append (T word)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
