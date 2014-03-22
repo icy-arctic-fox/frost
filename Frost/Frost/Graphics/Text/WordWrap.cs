4497b1a0caaaa27731c39321a05f1d989d9fd6d9
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-22 13:15:45 -0600

    Initial word wrap logic

--- Frost/Frost/Graphics/Text/WordWrap.cs
+++ Frost/Frost/Graphics/Text/WordWrap.cs
@@ -10,9 +10,12 @@ namespace Frost.Graphics.Text
 	/// </summary>
 	internal class WordWrap<T> where T : ITextSize
 	{
-		private readonly int _width;
+		private readonly int _targetWidth;
+		private int _curWidth, _curHeight; // Sizes of the current line
 		private IntRect _bounds;
+
 		private readonly LinkedList<LinkedList<T>> _lines = new LinkedList<LinkedList<T>>();
+		private LinkedList<T> _curLine = new LinkedList<T>();
 
 		// TODO: Add computation for horizontal alignment (left, center, right)
 
@@ -22,7 +25,8 @@ namespace Frost.Graphics.Text
 		/// <param name="width">Width to wrap each line to</param>
 		public WordWrap (int width)
 		{
-			_width = width;
+			_targetWidth = width;
+			_lines.AddLast(_curLine);
 		}
 
 		/// <summary>
@@ -44,7 +48,16 @@ namespace Frost.Graphics.Text
 		/// <summary>
 		/// Total number of lines of text after wrapping has been applied
 		/// </summary>
-		public int LineCount { get; private set; }
+		public int LineCount
+		{
+			get
+			{
+				var lines = _lines.Count;
+				if(_lines.Last.Value.Count <= 0)
+					--lines; // Current line has nothing on it, don't count it
+				return lines;
+			}
+		}
 
 		/// <summary>
 		/// Adds a unbreakable segment to the end of the text
@@ -52,7 +65,33 @@ namespace Frost.Graphics.Text
 		/// <param name="word">Segment to append to the text block</param>
 		public void Append (T word)
 		{
-			throw new NotImplementedException();
+			var width  = word.GetWidth();
+			var height = word.GetHeight();
+
+			if(_curWidth > 0 && _curWidth + width > _targetWidth)
+			{// Move to the next line, out of space on the current one
+				_curWidth = 0;
+				_curHeight = height;
+				_curLine = new LinkedList<T>();
+				_lines.AddLast(_curLine);
+			}
+			else
+			{// Stay on the same line
+				if(height > _curHeight)
+				{// Extend the height of the bounds
+					var diff = _curHeight - height;
+					_bounds.Height += diff;
+					_curHeight = height;
+				}
+			}
+
+			// Add the word to the current line
+			_curLine.AddLast(word);
+
+			// Extend the width of the line
+			_curWidth += width;
+			if(_curWidth > _bounds.Width)
+				_bounds.Width = _curWidth;
 		}
 	}
 }
