a8ffe5b5be300ac296d649765c2a336fa9740433
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-22 14:27:54 -0600

    Set up the structure

--- Frost/Frost/Graphics/Text/PlainTextRenderer.cs
+++ Frost/Frost/Graphics/Text/PlainTextRenderer.cs
@@ -17,10 +17,47 @@ namespace Frost.Graphics.Text
 		public string Text { get; set; }
 
 		/// <summary>
+		/// Retrieves the text to render and corrects it based on rendering properties
+		/// </summary>
+		/// <returns>Fixed text string</returns>
+		private string getFixedText ()
+		{
+			var text = Text ?? String.Empty;
+			if(!MultiLine)
+			{// Strip newline characters
+				text = text.Replace("\r", "");
+				text = text.Replace("\n", "");
+			}
+			return text;
+		}
+
+		/// <summary>
 		/// Calculates the bounds of the space that the text will occupy
 		/// </summary>
 		/// <returns>Width and height of the bounds</returns>
 		protected override Vector2u CalculateBounds ()
+		{
+			var text = getFixedText();
+			return WordWrap ? calculateWrappedBounds(text, WrapWidth) : calculateBounds(text);
+		}
+
+		/// <summary>
+		/// Calculates the bounds of some text that does not wrap multiple lines
+		/// </summary>
+		/// <param name="text">Text to calculate the bounds of</param>
+		/// <returns>Width and height of the bounds</returns>
+		private static Vector2u calculateBounds (string text)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Calculates the bounds of some text that has word wrapping applied to it
+		/// </summary>
+		/// <param name="text">Text to calculate the bounds of</param>
+		/// <param name="width">Target width to wrap lines by</param>
+		/// <returns>Width and height of the bounds</returns>
+		private static Vector2u calculateWrappedBounds (string text, uint width)
 		{
 			throw new NotImplementedException();
 		}
@@ -30,6 +67,31 @@ namespace Frost.Graphics.Text
 		/// </summary>
 		/// <param name="target">Texture to render to</param>
 		protected override void Draw (RenderTexture target)
+		{
+			var text = getFixedText();
+			if(WordWrap)
+				drawWrappedText(target, text, WrapWidth);
+			else
+				drawText(target, text);
+		}
+
+		/// <summary>
+		/// Draws the text without applying any word wrapping to it
+		/// </summary>
+		/// <param name="target">Texture to draw the text onto</param>
+		/// <param name="text">Text to render</param>
+		private static void drawText (RenderTexture target, string text)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Draws the text and applies word wrapping to it
+		/// </summary>
+		/// <param name="target">Texture to draw the text onto</param>
+		/// <param name="text">Text to render</param>
+		/// <param name="width">Target width to wrap lines by</param>
+		private static void drawWrappedText (RenderTexture target, string text, uint width)
 		{
 			throw new NotImplementedException();
 		}
