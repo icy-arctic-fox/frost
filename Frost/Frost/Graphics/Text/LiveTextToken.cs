94626980219fb5f25247ebaba58d046f94aa8322
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-27 16:05:25 -0600

    Created the live text token class

---
+++ Frost/Frost/Graphics/Text/LiveTextToken.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// A piece from a live text string
+	/// </summary>
+	internal class LiveTextToken
+	{
+		private readonly string _value;
+
+		/// <summary>
+		/// Creates a live text token
+		/// </summary>
+		/// <param name="value">String value of the token</param>
+		public LiveTextToken (string value)
+		{
+			if(value == null)
+				throw new ArgumentNullException("value");
+			_value = value;
+		}
+
+		/// <summary>
+		/// Textual representation of the token
+		/// </summary>
+		/// <returns>Token's string value</returns>
+		public override string ToString ()
+		{
+			return _value;
+		}
+	}
+}
