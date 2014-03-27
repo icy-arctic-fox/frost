2eb6f295f7465e7933f51594f4841de5d5fff001
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-27 16:36:18 -0600

    Added a token for that start of a formatting code

---
+++ Frost/Frost/Graphics/Text/LiveTextStartFormatToken.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// Live text token that symbolizes a formatting code (start of a formatted segment)
+	/// </summary>
+	internal class LiveTextStartFormatToken : LiveTextToken
+	{
+		private readonly string _formatName, _extra;
+
+		/// <summary>
+		/// Name of the type of formatting that will be applied
+		/// </summary>
+		public string FormatName
+		{
+			get { return _formatName; }
+		}
+
+		/// <summary>
+		/// Additional information used in formatting
+		/// </summary>
+		public string ExtraInfo
+		{
+			get { return _extra; }
+		}
+
+		/// <summary>
+		/// Creates a start formatting token
+		/// </summary>
+		/// <param name="token">Full string of the token</param>
+		/// <param name="formatName">Name of the formatting</param>
+		/// <param name="extra">Optional extra information used in formatting</param>
+		/// <exception cref="ArgumentNullException">The full string (<paramref name="token"/>) and <paramref name="formatName"/> can't be null.</exception>
+		public LiveTextStartFormatToken (string token, string formatName, string extra)
+			: base(token)
+		{
+			if(_formatName == null)
+				throw new ArgumentNullException("formatName");
+
+			_formatName = formatName;
+			_extra      = extra;
+		}
+	}
+}
