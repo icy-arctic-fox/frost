be6eea286a513ecdd1929396cd2a22c6ee0af100
Michael Miller <icy.arctic.fox@gmail.com>
2014-03-27 23:21:15 -0600

    Added a token for the end of a formatting code

---
+++ Frost/Frost/Graphics/Text/LiveTextEndFormatToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Frost.Graphics.Text
+{
+	/// <summary>
+	/// Represents the end of a formatting code sequence
+	/// </summary>
+	internal class LiveTextEndFormatToken : LiveTextToken
+	{
+		/// <summary>
+		/// Creates a live text token
+		/// </summary>
+		/// <param name="token">String representation of the entire token</param>
+		/// <exception cref="ArgumentNullException">The full string (<paramref name="token"/>) can't be null.</exception>
+		public LiveTextEndFormatToken (string token)
+			: base(token)
+		{
+			// ...
+		}
+	}
+}
