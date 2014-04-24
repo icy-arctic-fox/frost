cb27a4f7cc5c4096875c4c6a309b2f072fd43fed
Michael Miller <icy.arctic.fox@gmail.com>
2014-04-24 14:37:00 -0600

    Added functionality for translating formatting codes

--- Frost/Frost/Graphics/Text/LiveTextParser.cs
+++ Frost/Frost/Graphics/Text/LiveTextParser.cs
@@ -16,6 +16,15 @@ namespace Frost.Graphics.Text
 		private readonly Stack<TextAppearance> _appearanceStack = new Stack<TextAppearance>();
 
 		/// <summary>
+		/// Describes a method that translates formatting code information into changes in text appearance
+		/// </summary>
+		/// <param name="type">Name of the formatter</param>
+		/// <param name="extra">Extra information for formatting (will be null if omitted)</param>
+		/// <param name="before">Appearance of the text before the formatting is applied</param>
+		/// <returns>Appearance of the text after the formatting is applied</returns>
+		public delegate TextAppearance FormattingCodeTranslator (string type, string extra, TextAppearance before);
+
+		/// <summary>
 		/// Creates a new live text string parser
 		/// </summary>
 		/// <param name="text">Text containing the live text with formatting codes</param>
@@ -33,8 +42,9 @@ namespace Frost.Graphics.Text
 		/// <summary>
 		/// Parses the string that contains live text formatters and constructs a collection of segments that make up the live text
 		/// </summary>
+		/// <param name="translator">Method that will apply text appearance changes</param>
 		/// <returns>Live text segments pulled from the string</returns>
-		public IEnumerable<LiveTextSegment> Parse ()
+		public IEnumerable<LiveTextSegment> Parse (FormattingCodeTranslator translator)
 		{
 			var segments = new List<LiveTextSegment>();
 
@@ -43,7 +53,20 @@ namespace Frost.Graphics.Text
 			{
 				var startToken = token as LiveTextStartFormatToken;
 				if(startToken != null)
-					throw new NotImplementedException();
+				{// Start formatting, push the applied change onto the appearance stack
+					if(translator != null)
+					{// A translator is available, use it to generate the change in appearance
+						var before = _appearanceStack.Peek();
+						var current = translator(startToken.FormatName, startToken.ExtraInfo, before);
+						if(current != null)
+						{// Only add it if a change was made
+							_appearanceStack.Push(current);
+							continue;
+						}
+						// else - fallthrough...
+					}
+					// TODO: else - unrecognized formatter or no translator, put the literal text in
+				}
 				else
 				{
 					var endToken = token as LiveTextEndFormatToken;
