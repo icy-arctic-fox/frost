d5aeba666dea0cec77a297dee5169be8ed9178d2
Michael Miller <icy.arctic.fox@gmail.com>
2014-04-26 16:52:29 -0600

    Implemented CountLines()

--- Frost/Frost/Utility/StringUtility.cs
+++ Frost/Frost/Utility/StringUtility.cs
@@ -13,9 +13,40 @@ namespace Frost.Utility
 		/// </summary>
 		/// <param name="value">String to count the lines in</param>
 		/// <returns>Number of lines in the string</returns>
+		/// <remarks>-1 is returned if <paramref name="value"/> is null.
+		/// 0 is returned if <paramref name="value"/> is an empty string.</remarks>
 		public static int CountLines (this string value)
 		{
-			throw new NotImplementedException();
+			if(value == null)
+				return -1;
+			if(value == String.Empty)
+				return 0;
+
+			var lines = 1;
+			var cr = false;
+			for(var i = 0; i < value.Length; ++i)
+			{
+				var c = value[i];
+				switch(c)
+				{
+				case '\r':
+					cr = true;
+					++lines;
+					break;
+
+				case '\n':
+					if(!cr) // Increment if previous character wasn't \r
+						++lines;
+					cr = false;
+					break;
+
+				default:
+					cr = false;
+					break;
+				}
+			}
+
+			return lines;
 		}
 	}
 }
