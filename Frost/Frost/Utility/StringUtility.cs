cda53a472cb08f55da6bf49a01071f210a53ad97
Michael Miller <icy.arctic.fox@gmail.com>
2014-04-26 16:26:42 -0600

    Created a utility class for strings

---
+++ Frost/Frost/Utility/StringUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Frost.Utility
+{
+	/// <summary>
+	/// Useful functionality for when working with strings
+	/// </summary>
+	public static class StringUtility
+	{
+		/// <summary>
+		/// Counts the number of lines in a string
+		/// </summary>
+		/// <param name="value">String to count the lines in</param>
+		/// <returns>Number of lines in the string</returns>
+		public static int CountLines (this string value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
