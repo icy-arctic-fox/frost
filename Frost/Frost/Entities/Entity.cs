a6f63369af40eec45d97edb02aea134011729745
Michael Miller <icy.arctic.fox@gmail.com>
2014-04-29 14:41:42 -0600

    Added some initial structure for entity components

--- Frost/Frost/Entities/Entity.cs
+++ Frost/Frost/Entities/Entity.cs
@@ -1,10 +1,15 @@
-﻿namespace Frost.Entities
+﻿using System;
+using System.Collections.Generic;
+
+namespace Frost.Entities
 {
 	/// <summary>
 	/// Base game object that consists of a unique ID and component information
 	/// </summary>
 	public class Entity
 	{
+		#region ID
+
 		private ulong _id;
 
 		/// <summary>
@@ -25,5 +30,41 @@
 		{
 			get { return _id != 0L; }
 		}
+
+		/// <summary>
+		/// Sets the unique identification number of the entity
+		/// </summary>
+		/// <param name="id">Entity ID number</param>
+		internal void SetId (ulong id)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+		#region Components
+
+		private readonly List<IEntityComponent> _components = new List<IEntityComponent>(0);
+
+		/// <summary>
+		/// Adds component functionality to the entity
+		/// </summary>
+		/// <param name="index">Index in the component array to store the component at</param>
+		/// <param name="component">Component information</param>
+		/// <exception cref="InvalidOperationException">Components can't be added to the entity after it has been registered.</exception>
+		internal void AddComponent (int index, IEntityComponent component)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Retrieves component information from the entity
+		/// </summary>
+		/// <param name="index">Index in the component array that the component information is stored at</param>
+		/// <returns>Component information</returns>
+		internal IEntityComponent GetComponent (int index)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
 	}
 }
---
+++ Frost/Frost/Entities/IEntityComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Frost.Entities
+{
+	/// <summary>
+	/// Empty interface that serves as a base for all entity component types
+	/// </summary>
+	public interface IEntityComponent
+	{
+	}
+}
