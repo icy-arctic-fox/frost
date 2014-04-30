f7077637e74b079546ce4835bb4a0d47db1768b8
Michael Miller <icy.arctic.fox@gmail.com>
2014-04-29 19:42:05 -0600

    Added a subsystem interface

---
+++ Frost/Frost/Subsystems/ISubsystem.cs
@@ -0,0 +1,23 @@
+﻿using Frost.Entities;
+
+namespace Frost.Subsystems
+{
+	/// <summary>
+	/// Processes multiple entities by using their components
+	/// </summary>
+	public interface ISubsystem
+	{
+		/// <summary>
+		/// Processes a single entity
+		/// </summary>
+		/// <param name="entity">Entity to process</param>
+		void Process (Entity entity);
+
+		/// <summary>
+		/// Determines whether the subsystem can process the entity
+		/// </summary>
+		/// <param name="entity">Entity to check</param>
+		/// <returns>True if the entity can be processed by the subsystem</returns>
+		bool CanProcess (Entity entity);
+	}
+}
