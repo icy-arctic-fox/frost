2a9b089affc957c973ba5cddbfcda8d3413cf583
Michael Miller <icy.arctic.fox@gmail.com>
2014-04-29 20:04:21 -0600

    Created an entity group class

---
+++ Frost/Frost/Entities/EntityGroup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Frost.Entities
+{
+	/// <summary>
+	/// Collection of entities.
+	/// Disposed entities are automatically removed from the collection.
+	/// </summary>
+	public class EntityGroup : ICollection<Entity>
+	{
+		/// <summary>
+		/// Returns an enumerator that iterates through the group
+		/// </summary>
+		/// <returns>An enumerator that can be used to iterate through the group of entities</returns>
+		public IEnumerator<Entity> GetEnumerator ()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Returns an enumerator that iterates through the group
+		/// </summary>
+		/// <returns>An enumerator object that can be used to iterate through the group of entities</returns>
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return GetEnumerator();
+		}
+
+		/// <summary>
+		/// Adds an entity to the group
+		/// </summary>
+		/// <param name="item">The entity to add to the group</param>
+		/// <exception cref="NotSupportedException">The collection is read-only.</exception>
+		public void Add (Entity item)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Removes all entities from the group
+		/// </summary>
+		/// <exception cref="NotSupportedException">The collection is read-only.</exception>
+		public void Clear ()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Determines whether the group contains a specific entity
+		/// </summary>
+		/// <returns>True if <paramref name="item"/> is found in the group; otherwise, false</returns>
+		/// <param name="item">The entity to locate in the group</param>
+		public bool Contains (Entity item)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Copies the elements of the group to an <see cref="Array"/>, starting at a particular <see cref="Array"/> index
+		/// </summary>
+		/// <param name="array">The one-dimensional <see cref="Array"/> that is the destination of the elements copied from group -
+		/// the <see cref="Array"/> must have zero-based indexing.</param>
+		/// <param name="arrayIndex">The zero-based index in <paramref name="array"/> at which copying begins</param>
+		/// <exception cref="ArgumentNullException">The <paramref name="array"/> to copy to can't be null.</exception>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="arrayIndex"/> is less than 0.</exception>
+		/// <exception cref="ArgumentException">The number of elements in the source group is greater than the available space from <paramref name="arrayIndex"/> to the end of the destination <paramref name="array"/>.</exception>
+		public void CopyTo (Entity[] array, int arrayIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Removes the first occurrence of a specific entity from the group
+		/// </summary>
+		/// <returns>True if <paramref name="item"/> was successfully removed from the group; otherwise, false.
+		/// This method also returns false if <paramref name="item"/> is not found in the original group.</returns>
+		/// <param name="item">The entity to remove from the group</param>
+		/// <exception cref="NotSupportedException">The collection is read-only.</exception>
+		public bool Remove (Entity item)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Number of entities contained in the group
+		/// </summary>
+		public int Count
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Indicates whether the collection is read-only
+		/// </summary>
+		public bool IsReadOnly
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
+}
