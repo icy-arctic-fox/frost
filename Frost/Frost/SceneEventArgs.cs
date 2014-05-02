a48b68db8d74b5898e4a2f62d09f631e186e075b
Michael Miller <icy.arctic.fox@gmail.com>
2014-05-01 22:40:05 -0600

    Added a class for scene events

---
+++ Frost/Frost/SceneEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Frost
+{
+	/// <summary>
+	/// Describes an event that involves a scene
+	/// </summary>
+	public class SceneEventArgs : EventArgs
+	{
+		private readonly Scene _scene;
+
+		/// <summary>
+		/// Scene involved in the event
+		/// </summary>
+		public Scene Scene
+		{
+			get { return _scene; }
+		}
+
+		/// <summary>
+		/// Creates a new scene event
+		/// </summary>
+		/// <param name="scene">Scene involved in the event</param>
+		/// <exception cref="ArgumentNullException">The <paramref name="scene"/> can't be null.</exception>
+		public SceneEventArgs (Scene scene)
+		{
+			if(scene == null)
+				throw new ArgumentNullException("scene");
+
+			_scene = scene;
+		}
+	}
+}
