8f4f92d3224d18ffcb4f7852bb365de53a49c2f9
Michael Miller <icy.arctic.fox@gmail.com>
2014-05-03 09:46:27 -0600

    Added most (if not all) of the properties for a frame step event

---
+++ Frost/Frost/FrameStepEventArgs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Frost
+{
+	/// <summary>
+	/// Information for game logic update each frame
+	/// </summary>
+	/// <remarks>The contents of the class are internally mutable so that a single instance can be reused.</remarks>
+	public class FrameStepEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Index of the state that was updated last frame
+		/// </summary>
+		public int PreviousStateIndex { get; internal set; }
+
+		/// <summary>
+		/// Index of the state to update this frame
+		/// </summary>
+		public int NextStateIndex { get; internal set; }
+
+		/// <summary>
+		/// Current frame number
+		/// </summary>
+		public ulong Frame { get; internal set; }
+
+		/// <summary>
+		/// Length of time that the game has been running for
+		/// </summary>
+		public TimeSpan GameTime { get; internal set; }
+
+		/// <summary>
+		/// Indicates whether game updates are falling behind of the target update rate
+		/// </summary>
+		public bool IsRunningSlow { get; internal set; }
+	}
+}
