10261b60b236938623ba723ff787fa3fa143fbde
Michael Miller <icy.arctic.fox@gmail.com>
2014-05-03 11:32:50 -0600

    Changed some of the properties included for rendering info

--- Frost/Frost/FrameDrawEventArgs.cs
+++ Frost/Frost/FrameDrawEventArgs.cs
@@ -9,25 +9,30 @@ namespace Frost
 	public class FrameDrawEventArgs : EventArgs
 	{
 		/// <summary>
+		/// Index of the previously rendered state
+		/// </summary>
+		/// <remarks>The state that this index refers to cannot be used to retrieve information from.
+		/// An update process may be changing the state.</remarks>
+		public int PreviousStateIndex { get; internal set; }
+
+		/// <summary>
 		/// Index of the state to draw
 		/// </summary>
 		public int StateIndex { get; internal set; }
 
 		/// <summary>
-		/// Frame number currently being drawn
+		/// Indicates whether a frame is rendered more than once
 		/// </summary>
-		public long FrameNumber { get; internal set; }
+		public bool Duplicate
+		{
+			get { return PreviousStateIndex == StateIndex; }
+		}
 
 		/// <summary>
 		/// Fraction of time that has passed since the last frame update and the next one.
 		/// This is a number from 0 to 1, but can be greater than 1 if the renderer is ahead of game updates.
 		/// </summary>
 		public double Interpolation { get; internal set; }
-
-		/// <summary>
-		/// Length of time that the game has been running for
-		/// </summary>
-		public TimeSpan GameTime { get; internal set; }
 
 		/// <summary>
 		/// Indicates whether game updates are falling behind of the target update rate
