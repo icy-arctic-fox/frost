e02e53f4cd4bfbdf71dd489c1b773b1ef249a59c
Michael Miller <icy.arctic.fox@gmail.com>
2014-05-03 21:12:41 -0600

    Added a class for entity related events

---
+++ Frost/Frost/Entities/EntityEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Frost.Entities
+{
+	/// <summary>
+	/// Describes an event that involves an entity
+	/// </summary>
+	public class EntityEventArgs : EventArgs
+	{
+		private readonly Entity _entity;
+
+		/// <summary>
+		/// Entity involved with the event
+		/// </summary>
+		public Entity Entity
+		{
+			get { return _entity; }
+		}
+
+		/// <summary>
+		/// Creates an entity event description
+		/// </summary>
+		/// <param name="entity">Entity involved with the event</param>
+		/// <exception cref="ArgumentNullException">The <paramref name="entity"/> involved with the event can't be null.</exception>
+		public EntityEventArgs (Entity entity)
+		{
+			if(entity == null)
+				throw new ArgumentNullException("entity");
+
+			_entity = entity;
+		}
+	}
+}
