114f564a15a8cf0a888536cb19964449a6db74d6
Michael Miller <icy.arctic.fox@gmail.com>
2014-05-04 08:17:29 -0600

    Implemented the Iterate() method

--- Frost/Frost/Entities/EntityGroup.cs
+++ Frost/Frost/Entities/EntityGroup.cs
@@ -74,10 +74,19 @@ namespace Frost.Entities
 		/// Iterates over all of the entities and calls a method for each one
 		/// </summary>
 		/// <param name="method">Method to call for each entity</param>
+		/// <param name="arg">Additional argument to pass to the method</param>
 		/// <exception cref="ArgumentNullException">The <paramref name="method"/> to call for each entity can't be null.</exception>
-		public void Iterate<T> (Action<Entity, T> method)
+		public void Iterate<T> (Action<Entity, T> method, T arg)
 		{
-			throw new NotImplementedException();
+			if(method == null)
+				throw new ArgumentNullException("method");
+
+			lock(_entities)
+				for(var i = 0; i < _entities.Count; ++i)
+				{
+					var entity = _entities[i];
+					method(entity, arg);
+				}
 		}
 
 		/// <summary>
