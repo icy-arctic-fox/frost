b29e849fa539e60a3953f3ac18f2636eccf5fd0c
Michael Miller <icy.arctic.fox@gmail.com>
2014-05-13 21:48:54 -0600

    Added a component for 2D entities

---
+++ Frost/Frost/Entities/Positional2DComponent.cs
@@ -0,0 +1,18 @@
+﻿namespace Frost.Entities
+{
+	/// <summary>
+	/// Information about an entity's location in 2D space
+	/// </summary>
+	public class Positional2DComponent : IEntityComponent
+	{
+		/// <summary>
+		/// Offset of the entity from the origin along the x-axis
+		/// </summary>
+		public float X { get; set; }
+
+		/// <summary>
+		/// Offset of the entity from the origin along the y-axis
+		/// </summary>
+		public float Y { get; set; }
+	}
+}
