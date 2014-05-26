14e047d9979b4ed847136eda3cb797f3408bb0dd
Michael Miller <icy.arctic.fox@gmail.com>
2014-05-25 19:16:09 -0600

    Removing equality, hash, and string code
    Doesn't work with the upcoming change.

--- Frost/Frost/Entities/Positional2DComponent.cs
+++ Frost/Frost/Entities/Positional2DComponent.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Frost.Entities
+﻿namespace Frost.Entities
 {
 	/// <summary>
 	/// Information about an entity's location in 2D space
@@ -16,50 +14,5 @@ namespace Frost.Entities
 		/// Offset of the entity from the origin along the y-axis
 		/// </summary>
 		public float Y { get; set; }
-
-		/// <summary>
-		/// Compares another object to the current one to check if they're identical
-		/// </summary>
-		/// <param name="obj">Object to compare against</param>
-		/// <returns>True if <paramref name="obj"/> has identical values</returns>
-		/// <remarks>Null objects and instances that aren't of type <see cref="Positional2DComponent"/> are considered not equal.</remarks>
-		public override bool Equals (object obj)
-		{
-			if(ReferenceEquals(null, obj))
-				return false;
-			if(ReferenceEquals(this, obj))
-				return true;
-			var other = obj as Positional2DComponent;
-			return other != null && Equals(other);
-		}
-
-		/// <summary>
-		/// Compares another instance to the current one to check if they contain the same values
-		/// </summary>
-		/// <param name="other">Instance to compare against</param>
-		/// <returns>True if <paramref name="other"/> has identical values</returns>
-		protected bool Equals (Positional2DComponent other)
-		{
-			return X.Equals(other.X) && Y.Equals(other.Y);
-		}
-
-		/// <summary>
-		/// Generates a hash code from the values in the component
-		/// </summary>
-		/// <returns>A hash code</returns>
-		public override int GetHashCode ()
-		{
-			return unchecked((X.GetHashCode() * 397) ^ Y.GetHashCode());
-		}
-
-		/// <summary>
-		/// Creates the string representation of the the component
-		/// </summary>
-		/// <returns>A string in the form:
-		/// (Position2D X: ###, Y: ###)</returns>
-		public override string ToString ()
-		{
-			return String.Format("(Position2D X: {0:R}, Y: {1:R})", X, Y);
-		}
 	}
 }
