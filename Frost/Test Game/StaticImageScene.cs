c1a65df5105fe3c78d3147cd9ed37d2667b49883
Michael Miller <icy.arctic.fox@gmail.com>
2014-06-13 13:43:03 -0600

    Create static image scene to test ECS

---
+++ Frost/Test Game/StaticImageScene.cs
@@ -0,0 +1,26 @@
+﻿using Frost;
+using Frost.Entities;
+
+namespace Test_Game
+{
+	class StaticImageScene : Scene
+	{
+		public override string Name
+		{
+			get { return "Static Image"; }
+		}
+
+		public StaticImageScene ()
+		{
+			var factory = new EntityFactory(Entities);
+			factory.AddComponent(new Positional2DComponent());
+//			factory.AddComponent(new StaticImageComponent());
+			factory.Construct();
+		}
+
+		public override bool AllowFallthrough
+		{
+			get { return false; }
+		}
+	}
+}
