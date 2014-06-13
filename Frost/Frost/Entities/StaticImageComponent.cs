4258b80a7f7fabb989453bde157b622a42d34b24
Michael Miller <icy.arctic.fox@gmail.com>
2014-06-13 13:47:04 -0600

    Simple static image component for entities
    This component will most likely be changed or removed later. It is
    purely for testing right now.

---
+++ Frost/Frost/Entities/StaticImageComponent.cs
@@ -0,0 +1,37 @@
+﻿using Frost.Graphics;
+
+namespace Frost.Entities
+{
+	/// <summary>
+	/// Displays a static 2D image
+	/// </summary>
+	public class StaticImageComponent : IEntityComponent
+	{
+		private readonly Texture _texture;
+
+		/// <summary>
+		/// Creates a new component for an entity to exist in 2D space
+		/// </summary>
+		public StaticImageComponent (Texture texture)
+		{
+			_texture = texture;
+		}
+
+		/// <summary>
+		/// Texture displayed for the entity
+		/// </summary>
+		public Texture Texture
+		{
+			get { return _texture; }
+		}
+
+		/// <summary>
+		/// Creates a copy of the information in the component
+		/// </summary>
+		/// <returns>Copy of the component</returns>
+		public IEntityComponent CloneComponent ()
+		{
+			return new StaticImageComponent(_texture);
+		}
+	}
+}
