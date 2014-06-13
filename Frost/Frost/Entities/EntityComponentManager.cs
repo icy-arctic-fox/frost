7601e3ac2d68af73525fc5398a942f92c25f046d
Michael Miller <icy.arctic.fox@gmail.com>
2014-06-13 13:23:38 -0600

    Rename 'e' parameter to 'entity'

--- Frost/Frost/Entities/EntityComponentManager.cs
+++ Frost/Frost/Entities/EntityComponentManager.cs
@@ -29,43 +29,43 @@ namespace Frost.Entities
 		/// <summary>
 		/// Adds an empty entity (an entity with no components)
 		/// </summary>
-		/// <param name="e">Entity to add</param>
+		/// <param name="entity">Entity to add</param>
 		/// <exception cref="ArgumentNullException">The entity to add can't be null.</exception>
-		public void AddEntity (Entity e)
+		public void AddEntity (Entity entity)
 		{
-			if(e == null)
-				throw new ArgumentNullException("e");
+			if(entity == null)
+				throw new ArgumentNullException("entity");
 
-			extendComponentLists(e.Index);
+			extendComponentLists(entity.Index);
 		}
 
 		/// <summary>
 		/// Removes an entity and all of its components
 		/// </summary>
-		/// <param name="e">Entity to remove</param>
+		/// <param name="entity">Entity to remove</param>
 		/// <exception cref="ArgumentNullException">The entity to remove can't be null.</exception>
-		public void RemoveEntity (Entity e)
+		public void RemoveEntity (Entity entity)
 		{
-			if(e == null)
-				throw new ArgumentNullException("e");
+			if(entity == null)
+				throw new ArgumentNullException("entity");
 
-			removeComponentsAt(e.Index);
+			removeComponentsAt(entity.Index);
 		}
 
 		/// <summary>
 		/// Adds a component to an existing entity
 		/// </summary>
-		/// <param name="e">Entity to attach the component to</param>
+		/// <param name="entity">Entity to attach the component to</param>
 		/// <param name="component">Component to add to the entity</param>
 		/// <exception cref="ArgumentNullException">The entity and component can't be null.</exception>
-		public void AddComponent (Entity e, IEntityComponent component)
+		public void AddComponent (Entity entity, IEntityComponent component)
 		{
-			if(e == null)
-				throw new ArgumentNullException("e");
+			if(entity == null)
+				throw new ArgumentNullException("entity");
 			if(component == null)
 				throw new ArgumentNullException("component");
 
-			var entityIndex   = e.Index;
+			var entityIndex   = entity.Index;
 			var componentType = component.GetType();
 			var componentList = getComponentList(componentType, true);
 
@@ -77,29 +77,29 @@ namespace Frost.Entities
 		/// Retrieves a component from an entity
 		/// </summary>
 		/// <typeparam name="T">Type of component</typeparam>
-		/// <param name="e">Entity to retrieve the component of</param>
+		/// <param name="entity">Entity to retrieve the component of</param>
 		/// <returns>Corresponding component for the entity</returns>
 		/// <exception cref="ArgumentNullException">The entity can't be null.</exception>
-		public T GetComponent<T> (Entity e) where T : IEntityComponent
+		public T GetComponent<T> (Entity entity) where T : IEntityComponent
 		{
-			return (T)GetComponent(e, typeof(T));
+			return (T)GetComponent(entity, typeof(T));
 		}
 
 		/// <summary>
 		/// Retrieves a component from an entity
 		/// </summary>
-		/// <param name="e">Entity to retrieve the component of</param>
+		/// <param name="entity">Entity to retrieve the component of</param>
 		/// <param name="componentType">Type of component to retrieve</param>
 		/// <returns>Corresponding component for the entity</returns>
 		/// <exception cref="ArgumentNullException">The entity and component type can't be null.</exception>
-		public IEntityComponent GetComponent (Entity e, Type componentType)
+		public IEntityComponent GetComponent (Entity entity, Type componentType)
 		{
-			if(e == null)
-				throw new ArgumentNullException("e");
+			if(entity == null)
+				throw new ArgumentNullException("entity");
 			if(componentType == null)
 				throw new ArgumentNullException("componentType");
 
-			var entityIndex   = e.Index;
+			var entityIndex   = entity.Index;
 			var componentList = getComponentList(componentType);
 
 			return (componentList != null) ? componentList[entityIndex] : null;
@@ -109,29 +109,29 @@ namespace Frost.Entities
 		/// Removes a component from an existing entity
 		/// </summary>
 		/// <typeparam name="T">Type of component to remove from the entity</typeparam>
-		/// <param name="e">Entity to detach the component from</param>
+		/// <param name="entity">Entity to detach the component from</param>
 		/// <returns>True if the component was removed or false if the entity is untracked or the entity doesn't have the component</returns>
 		/// <exception cref="ArgumentNullException">The entity can't be null.</exception>
-		public bool RemoveComponent<T> (Entity e)
+		public bool RemoveComponent<T> (Entity entity)
 		{
-			return RemoveComponent(e, typeof(T));
+			return RemoveComponent(entity, typeof(T));
 		}
 
 		/// <summary>
 		/// Removes a component from an existing entity
 		/// </summary>
-		/// <param name="e">Entity to detach the component from</param>
+		/// <param name="entity">Entity to detach the component from</param>
 		/// <param name="componentType">Type of component to remove from the entity</param>
 		/// <returns>True if the component was removed or false if the entity is untracked or the entity doesn't have the component</returns>
 		/// <exception cref="ArgumentNullException">The entity and component type can't be null.</exception>
-		public bool RemoveComponent (Entity e, Type componentType)
+		public bool RemoveComponent (Entity entity, Type componentType)
 		{
-			if(e == null)
-				throw new ArgumentNullException("e");
+			if(entity == null)
+				throw new ArgumentNullException("entity");
 			if(componentType == null)
 				throw new ArgumentNullException("componentType");
 
-			var entityIndex   = e.Index;
+			var entityIndex   = entity.Index;
 			var componentList = getComponentList(componentType);
 
 			if(componentList != null)
@@ -152,29 +152,29 @@ namespace Frost.Entities
 		/// Checks whether an entity has a component
 		/// </summary>
 		/// <typeparam name="T">Type of component</typeparam>
-		/// <param name="e">Entity to check</param>
+		/// <param name="entity">Entity to check</param>
 		/// <returns>True if the entity has the component, false otherwise</returns>
 		/// <exception cref="ArgumentNullException">The entity can't be null.</exception>
-		public bool HasComponent<T> (Entity e) where T : IEntityComponent
+		public bool HasComponent<T> (Entity entity) where T : IEntityComponent
 		{
-			return HasComponent(e, typeof(T));
+			return HasComponent(entity, typeof(T));
 		}
 
 		/// <summary>
 		/// Checks whether an entity has a component
 		/// </summary>
-		/// <param name="e">Entity to check</param>
+		/// <param name="entity">Entity to check</param>
 		/// <param name="componentType">Type of component</param>
 		/// <returns>True if the entity has the component, false otherwise</returns>
 		/// <exception cref="ArgumentNullException">The entity and component type can't be null.</exception>
-		public bool HasComponent (Entity e, Type componentType)
+		public bool HasComponent (Entity entity, Type componentType)
 		{
-			if(e == null)
-				throw new ArgumentNullException("e");
+			if(entity == null)
+				throw new ArgumentNullException("entity");
 			if(componentType == null)
 				throw new ArgumentNullException("componentType");
 
-			var entityIndex   = e.Index;
+			var entityIndex   = entity.Index;
 			var componentList = getComponentList(componentType);
 
 			if(componentList != null) // Component is known about
