2c1a241e1e66b6e29056b452ba21e48eb19d730c
Michael Miller <icy.arctic.fox@gmail.com>
2014-06-16 13:47:44 -0600

    Added a method for correcting angles in degrees

--- Frost/Frost/Utility/MathHelper.cs
+++ Frost/Frost/Utility/MathHelper.cs
@@ -5,5 +5,17 @@
 	/// </summary>
 	public static class MathHelper
 	{
+		/// <summary>
+		/// Corrects an angle in degrees so that it is between 0 and 360
+		/// </summary>
+		/// <param name="angle">Angle to correct</param>
+		/// <returns>Corrected angle</returns>
+		public static float CorrectAngle (float angle)
+		{
+			var corrected = angle % 360;
+			if(corrected < 0)
+				corrected += 360;
+			return corrected;
+		}
 	}
 }
