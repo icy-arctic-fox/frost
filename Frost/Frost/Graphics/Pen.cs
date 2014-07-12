9634bc915ace925fefd22c9aa3891d3818b181fc
Michael Miller <icy.arctic.fox@gmail.com>
2014-07-11 23:00:20 -0600

    Added a pen class for line info

---
+++ Frost/Frost/Graphics/Pen.cs
@@ -0,0 +1,40 @@
+﻿namespace Frost.Graphics
+{
+	/// <summary>
+	/// Information about a line's appearance
+	/// </summary>
+	public class Pen
+	{
+		private readonly Color _color;
+		private readonly float _width;
+
+		/// <summary>
+		/// Color of the line
+		/// </summary>
+		public Color Color
+		{
+			get { return _color; }
+		}
+
+		/// <summary>
+		/// Thickness of the line (in pixels)
+		/// </summary>
+		public float Width
+		{
+			get { return _width; }
+		}
+
+		/// <summary>
+		/// Creates a pen
+		/// </summary>
+		/// <param name="color">Color of the pen</param>
+		/// <param name="width">Thickness of the pen</param>
+		public Pen (Color color, float width)
+		{
+			_color = color;
+			_width = width;
+		}
+
+		// TODO: Style
+	}
+}
