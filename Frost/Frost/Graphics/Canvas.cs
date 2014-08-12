e3fc4423404bc684a57c2d4b414137bdbd2f3bae
Michael Miller <icy.arctic.fox@gmail.com>
2014-08-12 13:38:50 -0600

    Changed the pen to a property and added a constructor

--- Frost/Frost/Graphics/Canvas.cs
+++ Frost/Frost/Graphics/Canvas.cs
@@ -11,6 +11,20 @@ namespace Frost.Graphics
 	/// </summary>
 	public class Canvas // TODO: Implement IRenderTarget
 	{
-		private Pen _pen = Pen.Default;
+		/// <summary>
+		/// Pen used to draw lines
+		/// </summary>
+		/// <remarks>A "null" pen means no line will be drawn.</remarks>
+		public Pen Pen { get; set; }
+
+		/// <summary>
+		/// Creates a new canvas
+		/// </summary>
+		/// <param name="width">Width of the drawable region in pixels</param>
+		/// <param name="height">Height of the drawable region in pixels</param>
+		public Canvas (uint width, uint height)
+		{
+			Pen = Pen.Default;
+		}
 	}
 }
