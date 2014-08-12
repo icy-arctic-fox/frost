feb5a68015b52afa4c09b0f64254d0c9dc84fbe3
Michael Miller <icy.arctic.fox@gmail.com>
2014-08-12 13:34:31 -0600

    Removing IRenderTarget implementation for now

--- Frost/Frost/Graphics/Canvas.cs
+++ Frost/Frost/Graphics/Canvas.cs
@@ -9,37 +9,8 @@ namespace Frost.Graphics
 	/// <summary>
 	/// Bitmap capable of drawing various shapes and objects onto
 	/// </summary>
-	public class Canvas : IRenderTarget
+	public class Canvas // TODO: Implement IRenderTarget
 	{
 		private Pen _pen = Pen.Default;
-
-		/// <summary>
-		/// Draws an object to the canvas
-		/// </summary>
-		/// <param name="drawable">Object to draw</param>
-		public void Draw (Drawable drawable)
-		{
-			throw new NotImplementedException();
-		}
-
-		/// <summary>
-		/// Draws an object to the canvas with additional transformation and render options
-		/// </summary>
-		/// <param name="drawable">Object to draw</param>
-		/// <param name="transform">Additional transformation and render options</param>
-		public void Draw (Drawable drawable, RenderStates transform)
-		{
-			throw new NotImplementedException();
-		}
-
-		/// <summary>
-		/// Draws a collection of vertices with additional transformation and render options
-		/// </summary>
-		/// <param name="verts">Vertices to draw</param>
-		/// <param name="transform">Additional transformation and render options</param>
-		public void Draw (VertexArray verts, RenderStates transform)
-		{
-			throw new NotImplementedException();
-		}
 	}
 }
