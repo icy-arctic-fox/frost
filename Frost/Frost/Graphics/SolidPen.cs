53cab981e1dcd74255dfe2168c50881f8fe8bda5
Michael Miller <icy.arctic.fox@gmail.com>
2014-08-12 16:32:21 -0600

    Added the ConstructLineVertices() method

--- Frost/Frost/Graphics/SolidPen.cs
+++ Frost/Frost/Graphics/SolidPen.cs
@@ -1,4 +1,7 @@
-﻿namespace Frost.Graphics
+﻿using Frost.Geometry;
+using SFML.Graphics;
+
+namespace Frost.Graphics
 {
 	/// <summary>
 	/// Pen that produces a solid, single-color line
@@ -33,6 +36,19 @@
 		{
 			_color = color;
 			_thickness = thickness;
+		}
+
+		/// <summary>
+		/// Creates the vertices needed to represent the line
+		/// </summary>
+		/// <param name="v">Length and rotation of the line</param>
+		/// <returns>A collection of vertices that can be used to draw the line</returns>
+		/// <remarks>The pen's implementation for this method should not rotate the vertices.
+		/// Rotation is done by the calling method.
+		/// A rotation value is provided for cases where the pen type might want to adjust the appearance based on rotation.</remarks>
+		internal override VertexArray ConsructLineVertices (Vector2 v)
+		{
+			throw new System.NotImplementedException();
 		}
 	}
 }
