e1f62dfa939abc7350f7190c9a4c09d7b5a2c3bf
Michael Miller <icy.arctic.fox@gmail.com>
2014-08-12 15:27:42 -0600

    Added placeholders for drawing methods

--- Frost/Frost/Graphics/Canvas.cs
+++ Frost/Frost/Graphics/Canvas.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SFML.Graphics;
+using Frost.Geometry;
 
 namespace Frost.Graphics
 {
@@ -26,5 +23,167 @@ namespace Frost.Graphics
 		{
 			Pen = Pen.Default;
 		}
+
+		#region Drawing methods
+
+		/// <summary>
+		/// Draws a line between two points using the current pen
+		/// </summary>
+		/// <param name="start">First point to draw from</param>
+		/// <param name="end">Second point to draw to</param>
+		public void DrawLine (Point2i start, Point2i end)
+		{
+			DrawLine(start.X, start.Y, end.X, end.Y);
+		}
+
+		/// <summary>
+		/// Draws a line between to points using the current pen
+		/// </summary>
+		/// <param name="startX">X-coordinate of the first point to draw from</param>
+		/// <param name="startY">Y-coordinate of the first point to draw from</param>
+		/// <param name="endX">X-coordinate of the second point to draw to</param>
+		/// <param name="endY">Y-coordinate of the second point to draw to</param>
+		public void DrawLine (int startX, int startY, int endX, int endY)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Draws a multiple lines between points using the current pen
+		/// </summary>
+		/// <param name="points">Set of ordered points to draw lines between</param>
+		public void DrawLines (Point2i[] points)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Draws a rectangle with the current pen and brush
+		/// </summary>
+		/// <param name="origin">Top-left corner of the rectangle</param>
+		/// <param name="size">Size of the rectangle</param>
+		public void DrawRect (Point2i origin, Size2 size)
+		{
+			DrawRect(origin.X, origin.Y, size.Width, size.Height);
+		}
+
+		/// <summary>
+		/// Draws a rectangle with the current pen and brush
+		/// </summary>
+		/// <param name="x">X-coordinate of the top-left corner of the rectangle</param>
+		/// <param name="y">Y-coordinate of the top-left corner of the rectangle</param>
+		/// <param name="width">Width of the rectangle</param>
+		/// <param name="height">Height of the rectangle</param>
+		public void DrawRect (int x, int y, uint width, uint height)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Draws a rectangle with rounded corners using the current pen and brush
+		/// </summary>
+		/// <param name="origin">Top-left corner of the rectangle</param>
+		/// <param name="size">Size of the rectangle</param>
+		/// <param name="radius">Radius of the rounded corners</param>
+		/// <param name="precision">Percentage, from 0 to 1, indicating the smoothness of the rounded corners (higher is better quality, but slower)</param>
+		public void DrawRoundRect (Point2i origin, Size2 size, uint radius, float precision = 1f)
+		{
+			DrawRoundRect(origin.X, origin.Y, size.Width, size.Height, radius, precision);
+		}
+
+		/// <summary>
+		/// Draws a rectangle with rounded corners using the current pen and brush
+		/// </summary>
+		/// <param name="x">X-coordinate of the top-left corner of the rectangle</param>
+		/// <param name="y">Y-coordinate of the top-left corner of the rectangle</param>
+		/// <param name="width">Width of the rectangle</param>
+		/// <param name="height">Height of the rectangle</param>
+		/// <param name="radius">Radius of the rounded corners</param>
+		/// <param name="precision">Percentage, from 0 to 1, indicating the smoothness of the rounded corners (higher is better quality, but slower)</param>
+		public void DrawRoundRect (int x, int y, uint width, uint height, uint radius, float precision = 1f)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Draws a circle using the current pen and brush
+		/// </summary>
+		/// <param name="center">Position of the center of the circle</param>
+		/// <param name="radius">Distance from the center to the edge</param>
+		/// <param name="precision">Percentage, from 0 to 1, indicating the smoothness of the circle (higher is better quality, but slower)</param>
+		public void DrawCirlce (Point2i center, uint radius, float precision = 1f)
+		{
+			DrawCircle(center.X, center.Y, radius, precision);
+		}
+
+		/// <summary>
+		/// Draws a circle using the current pen and brush
+		/// </summary>
+		/// <param name="x">X-coordinate of the center of the circle</param>
+		/// <param name="y">Y-coordinate of the center of the circle</param>
+		/// <param name="radius">Distance from the center to the edge</param>
+		/// <param name="precision">Percentage, from 0 to 1, indicating the smoothness of the circle (higher is better quality, but slower)</param>
+		public void DrawCircle (int x, int y, uint radius, float precision = 1f)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Draws an ellipse using the current pen and brush
+		/// </summary>
+		/// <param name="center">Position of the center of the ellipse</param>
+		/// <param name="radii">Horizontal and vertical distance from the center to the corresponding edge</param>
+		/// <param name="precision">Percentage, from 0 to 1, indicating the smoothness of the ellipse (higher is better quality, but slower)</param>
+		public void DrawEllipse (Point2i center, Size2 radii, float precision = 1f)
+		{
+			DrawEllipse(center.X, center.Y, radii.Width, radii.Height, precision);
+		}
+
+		/// <summary>
+		/// Draws an ellipse using the current pen and brush
+		/// </summary>
+		/// <param name="x">X-coordinate of the center of the ellipse</param>
+		/// <param name="y">Y-coordinate of the center of the ellipse</param>
+		/// <param name="radiusX">Horizontal distance from the center to the edge</param>
+		/// <param name="radiusY">Vertical distance from the center to the edge</param>
+		/// <param name="precision">Percentage, from 0 to 1, indicating the smoothness of the ellipse (higher is better quality, but slower)</param>
+		public void DrawEllipse (int x, int y, uint radiusX, uint radiusY, float precision = 1f)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Draws an n-sided shape with equal side lengths using the current pen and brush
+		/// </summary>
+		/// <param name="center">Position of the center of the shape</param>
+		/// <param name="radius">Distance from the center to an edge</param>
+		/// <param name="sides">Number of sides on the shape</param>
+		public void DrawNGon (Point2i center, uint radius, uint sides)
+		{
+			DrawNGon(center.X, center.Y, radius, sides);
+		}
+
+		/// <summary>
+		/// Draws an n-sided shape with equal side lengths using the current pen and brush
+		/// </summary>
+		/// <param name="x">X-coordinate of the center of the shape</param>
+		/// <param name="y">Y-coordinate of the center of the shape</param>
+		/// <param name="radius">Distance from the center to an edge</param>
+		/// <param name="sides">Number of sides on the shape</param>
+		public void DrawNGon (int x, int y, uint radius, uint sides)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Draws an enclosed polygon using the current pen and brush
+		/// </summary>
+		/// <param name="points">Points to draw lines between</param>
+		/// <remarks>A line will be drawn from the last point in <paramref name="points"/> to the first point in <paramref name="points"/> to make an enclosed shape.</remarks>
+		public void DrawPolygon (Point2i[] points)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
 	}
 }
