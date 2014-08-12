9da80db58e0ea348babcdadca3a2b5d6d56b5798
Michael Miller <icy.arctic.fox@gmail.com>
2014-08-12 13:29:05 -0600

    Made Pen abstract and created a solid pen

--- Frost/Frost/Graphics/Pen.cs
+++ Frost/Frost/Graphics/Pen.cs
@@ -3,43 +3,11 @@
 	/// <summary>
 	/// Information about a line's appearance
 	/// </summary>
-	public class Pen
+	public abstract class Pen
 	{
 		/// <summary>
 		/// Default pen type
 		/// </summary>
-		public static readonly Pen Default = new Pen(new Color(), 1f);
-
-		private readonly Color _color;
-		private readonly float _width;
-
-		/// <summary>
-		/// Color of the line
-		/// </summary>
-		public Color Color
-		{
-			get { return _color; }
-		}
-
-		/// <summary>
-		/// Thickness of the line (in pixels)
-		/// </summary>
-		public float Width
-		{
-			get { return _width; }
-		}
-
-		/// <summary>
-		/// Creates a pen
-		/// </summary>
-		/// <param name="color">Color of the pen</param>
-		/// <param name="width">Thickness of the pen</param>
-		public Pen (Color color, float width)
-		{
-			_color = color;
-			_width = width;
-		}
-
-		// TODO: Style
+		public static readonly Pen Default = new SolidPen(new Color(), 1f);
 	}
 }
---
+++ Frost/Frost/Graphics/SolidPen.cs
@@ -0,0 +1,38 @@
+﻿namespace Frost.Graphics
+{
+	/// <summary>
+	/// Pen that produces a solid, single-color line
+	/// </summary>
+	public class SolidPen : Pen
+	{
+		private readonly Color _color;
+		private readonly float _width;
+
+		/// <summary>
+		/// Color of the line
+		/// </summary>
+		public Color Color
+		{
+			get { return _color; }
+		}
+
+		/// <summary>
+		/// Thickness of the line (in pixels)
+		/// </summary>
+		public float Width
+		{
+			get { return _width; }
+		}
+
+		/// <summary>
+		/// Creates a pen
+		/// </summary>
+		/// <param name="color">Color of the pen</param>
+		/// <param name="width">Thickness of the pen</param>
+		public SolidPen (Color color, float width)
+		{
+			_color = color;
+			_width = width;
+		}
+	}
+}
